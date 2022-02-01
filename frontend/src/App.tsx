f36cbbd2de44b93f0cf415ad093d06091ccd7e86
Tsai-Chieh <d0177381@gmail.com>
2022-02-01 23:59:20 +0800

    :alembic: try to call API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test';
+import axios from 'axios';
 
 function App() {
+
+  const getData = async () => {
+    const res = await axios.get('http://localhost:8080');
+    const {data} = res;
+    console.log(data);
+  };
+  getData()
   return (
     <div className="App">
       <header className="App-header">
+        <Test />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
