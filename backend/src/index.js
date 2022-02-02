77c20aa2f96560caab04f6f5bb027452b5e325c6
loki40316 <loki40316@gmail.com>
2022-02-02 15:55:53 +0800

    :alembic: try to solve CORS problem

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ const corsOptions = {
   ],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  // credentials: true
 };
 
 const app = express();
