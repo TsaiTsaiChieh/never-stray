ea69d62f82b09156e494dd19d235d5f15cdd229a
Tsai-Chieh <d0177381@gmail.com>
2022-02-10 14:30:29 +0800

    :see_no_evil: .gitignore is now working

--- backend/build/controllers/ping.controller.d.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from 'express';
-export declare class PingController {
-    pingAPI(req: Request, res: Response): Response<any, Record<string, any>>;
-}
-//# sourceMappingURL=ping.controller.d.ts.map
