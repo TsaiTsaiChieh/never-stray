8e41431e7a509c56882e54679924ef47cb3dd250
Tsai-Chieh <d0177381@gmail.com>
2022-02-10 19:06:37 +0800

    :sparkles: add ping db API

--- backend/src/controllers/ping.controller.ts
+++ backend/src/controllers/ping.controller.ts
@@ -1,6 +1,7 @@
 /* eslint-disable require-jsdoc */
 import {Request, Response} from 'express'
 import {Controller, Get, Req, Res} from 'routing-controllers'
+import {getConnectionManager} from 'typeorm'
 
 @Controller('/ping')
 
@@ -8,5 +9,22 @@ export class PingController {
   @Get('/api')
   pingAPI(@Req() req: Request, @Res() res: Response) {
     return res.send('pong')
+  }
+  @Get('/db')
+  async pingDB(@Req() req: Request, @Res() res: Response) {
+    const con = getConnectionManager().get()
+    try {
+      let canConnect: boolean = false
+      // Try to connect
+      if (!con.isConnected) await con.connect()
+      // Store connection result
+      canConnect = con.isConnected
+      if (!canConnect) return res.send('Database connection failed')
+      else return res.send('Database connection successfully')
+    } catch (error) {
+      return res.send(`Database error happened: ${error}`)
+    } finally {
+      await con.close()
+    }
   }
 }
