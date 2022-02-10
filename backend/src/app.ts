b09e7e486c0dc00ddebae750718044bcab7a9615
Tsai-Chieh <d0177381@gmail.com>
2022-02-10 22:23:24 +0800

    :loud_sound: add log

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -39,7 +39,7 @@ export class App {
   }
   /** Add DB */
   private async db(): Promise<void> {
-    console.log('----', process.env.TYPEORM_HOST, '----')
+    console.log('!!!----', process.env.TYPEORM_HOST, '----!!!')
     await createConnection({
       type: 'mysql',
       host: process.env.TYPEORM_HOST,
