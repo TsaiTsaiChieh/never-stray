cf1097a32cc61a476d87c06d964c89090f42140c
Tsai-Chieh <d0177381@gmail.com>
2022-02-13 00:31:30 +0800

    :zap: format and add Entity decorator

--- backend/src/entity/shelter.entity.ts
+++ backend/src/entity/shelter.entity.ts
@@ -1,10 +1,11 @@
-import {Column, PrimaryGeneratedColumn} from 'typeorm'
+import {Column, Entity, PrimaryGeneratedColumn} from 'typeorm'
 
+@Entity()
 /** @class Shelter */
 export class Shelter {
-    @PrimaryGeneratedColumn()
-    id?: number
+  @PrimaryGeneratedColumn()
+  id?: number
 
-    @Column({type: 'varchar', length: 32, nullable: false})
-    name: string
+  @Column({type: 'varchar', length: 32, nullable: false})
+  name: string
 }
