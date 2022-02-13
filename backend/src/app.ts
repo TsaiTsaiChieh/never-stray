e248ae46db10833c331cee8e9522f4c66b665d2f
Tsai-Chieh <d0177381@gmail.com>
2022-02-13 19:31:45 +0800

    :recycle: db load from database config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,10 +4,7 @@ import cors from 'cors'
 import express, {Application} from 'express'
 
 import {Router} from './router'
-import {createConnection} from 'typeorm'
-import {Area} from './entity/area.entity'
-import {Pet} from './entity/pet.entity'
-import {Shelter} from './entity/shelter.entity'
+import {connection} from './config/database'
 
 /** @class App */
 export class App {
@@ -42,15 +39,7 @@ export class App {
   }
   /** Add DB */
   private async db(): Promise<void> {
-    await createConnection({
-      type: 'mysql',
-      host: process.env.TYPEORM_HOST,
-      port: 3306,
-      username: process.env.MYSQL_USERNAME,
-      password: process.env.MYSQL_ROOT_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
-      entities: [Area, Pet, Shelter],
-    })
+    await connection()
   }
   /** Add router */
   private router(): void {
