6c5d442cd0ed7d02669d29cbb02f0db2742276bb
Tsai-Chieh <d0177381@gmail.com>
2022-02-15 21:48:54 +0800

    :recycle: refactor with try catch block

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -4,18 +4,24 @@ import {Connection, createConnection} from 'typeorm'
 import {Area} from '../entity/area.entity'
 import {Pet} from '../entity/pet.entity'
 import {Shelter} from '../entity/shelter.entity'
+import {DBError} from '../utils/app-error'
 
 export const connection = async (
   env: string = process.env.NODE_ENV,
 ): Promise<Connection> => {
   dotenv.config({path: `.env.${env}`})
-  return createConnection({
-    type: 'mysql',
-    host: process.env.TYPEORM_HOST,
-    port: 3306,
-    username: process.env.MYSQL_USERNAME,
-    password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    entities: [Area, Pet, Shelter],
-  })
+  try {
+    const connection = await createConnection({
+      type: 'mysql',
+      host: process.env.TYPEORM_HOST,
+      port: 3306,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_ROOT_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [Area, Pet, Shelter],
+    })
+    return Promise.resolve(connection)
+  } catch (error) {
+    return Promise.reject(new DBError())
+  }
 }
