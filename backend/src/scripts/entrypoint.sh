2b484b206179346c9c3297db6698828020b44e03
Tsai-Chieh <d0177381@gmail.com>
2022-02-18 20:17:43 +0800

    :wrench: modulize init enum table script

--- backend/src/scripts/entrypoint.sh
+++ backend/src/scripts/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 echo "Run migration ..."
 yarn typeorm:run
-echo "Initializing area data ..."
-yarn init:area
-echo "Initializing shelter data ..."
-yarn init:shelter
+echo "Initializing area & shelter data ..."
+yarn init:enum-table
 echo "===== END ====="
