a09d7a8a7174727a96547897d5bc2adfb6f10170
Tsai-Chieh <d0177381@gmail.com>
2022-02-21 20:26:22 +0800

    :recycle: rm useless async

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ const env: string = process.env.NODE_ENV!
 dotenv.config({path: `.env.${env}`})
 const APP_PORT: number = parseInt(process.env.APP_PORT!);
 
-(async () => {
+(() => {
   const app = App.bootstrap().app
   app.listen(APP_PORT, (): void => {
     console.info(`[${env}] Never-Stray App listening on ${APP_PORT}`)
