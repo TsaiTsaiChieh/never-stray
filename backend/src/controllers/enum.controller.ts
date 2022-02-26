dfce48e33951503cdd7f3c5f843930d39d70cb04
Tsai-Chieh <d0177381@gmail.com>
2022-02-27 00:17:18 +0800

    :sparkles: add GET /shelter API

--- backend/src/controllers/enum.controller.ts
+++ backend/src/controllers/enum.controller.ts
@@ -3,18 +3,34 @@ import {Request, Response} from 'express'
 import {Controller, Get, Req, Res} from 'routing-controllers'
 import safeAwait from 'safe-await'
 import {Area} from '../entity/area.entity'
+import {Shelter} from '../entity/shelter.entity'
 import {AreaModel} from '../models/area.model'
+import {ShelterModel} from '../models/shelter.model'
+
 
 @Controller('/enum')
 export class EnumController {
-  private model: AreaModel
+  private areaModel: AreaModel
+  private shelterModel: ShelterModel
+
   constructor() {
-    this.model = new AreaModel()
+    this.areaModel = new AreaModel()
+    this.shelterModel = new ShelterModel()
   }
+
   @Get('/city')
   async getCities(@Req() req: Request, @Res() res: Response) {
     const [error, result]: [ErrorType, Area[]] = await safeAwait(
-      this.model.getAll(),
+      this.areaModel.getAll(),
+    )
+    if (error) return res.status(error.code).json(error)
+    return res.json(result)
+  }
+
+  @Get('/shelter')
+  async getShelters(@Req() req: Request, @Res() res: Response) {
+    const [error, result]: [ErrorType, Shelter[]] = await safeAwait(
+      this.shelterModel.getAll(),
     )
     if (error) return res.status(error.code).json(error)
     return res.json(result)
