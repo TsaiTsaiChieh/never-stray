670819cdd2f9c0a4d28aa74332ef6bd9e08e2b1b
Tsai-Chieh <d0177381@gmail.com>
2022-02-26 21:37:14 +0800

    :sparkles: add AreaType

--- backend/src/typings/area.d.ts
+++ backend/src/typings/area.d.ts
@@ -1,5 +1,11 @@
 type AreaRegionType = 'E' | 'W' | 'S' | 'N' | 'M'
 
+type AreaType = {
+  id: number
+  region: AreaRegionType
+  name: string
+}
+
 type AreaDataType = IDNameType & {
   region: string
 }
