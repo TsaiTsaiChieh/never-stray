cfb1872bde8d4d69201934de217415b9499ebaf0
Tsai-Chieh <d0177381@gmail.com>
2022-02-28 13:35:42 +0800

    :construction_worker: add nginx service

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -2,3 +2,4 @@ echo "After install"
 cd /home/ubuntu/never-stray
 docker-compose build frontend
 docker-compose build backend
+docker-compose build nginx
--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -2,4 +2,5 @@ echo "Start never-stray docker"
 cd /home/ubuntu/never-stray
 docker-compose up -d frontend
 docker-compose up -d backend
+docker-compose up -d ngixn
 docker-compose up -d cron
