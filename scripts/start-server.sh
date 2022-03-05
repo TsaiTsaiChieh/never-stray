d8a0619949b89804959cf5a6a19c5f04ffba2aed
Tsai-Chieh <d0177381@gmail.com>
2022-03-05 17:23:00 +0800

    :construction_worker: run docker-compose with compatibility

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,6 +1,3 @@
 echo "Start never-stray docker"
 cd /home/ubuntu/never-stray
-docker-compose up -d frontend
-docker-compose up -d backend
-docker-compose up -d nginx
-docker-compose up -d cron
+docker-compose --compatibility up -d
