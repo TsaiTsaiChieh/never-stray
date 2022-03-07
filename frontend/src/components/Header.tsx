3e66a3797b7ca09c98a4b75988278956986f6631
Tsai-Chieh <d0177381@gmail.com>
2022-03-07 21:07:15 +0800

    :tada: init Header component

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div id="Header">
+      <div className="logo-group">
+        <img className="svg logo" src="images/logo-small.svg" alt="logo" />
+        <div className="slogan">
+          <h3 className="slogan-ch">讓愛不流浪</h3>
+          <h3 className="slogan-en">Never Stray</h3>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
