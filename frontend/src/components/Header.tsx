787d68dee2dbaf08d23d989030a53b848c1042cf
Tsai-Chieh <d0177381@gmail.com>
2022-03-08 18:36:33 +0800

    :fire: rm useless import & rename className

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-
 const Header = () => {
   return (
     <div id="Header">
       <div className="logo-group">
-        <img className="svg logo" src="images/logo-small.svg" alt="logo" />
+        <img className="logo-svg" src="images/logo-small.svg" alt="logo" />
         <div className="slogan">
           <h3 className="slogan-ch">讓愛不流浪</h3>
           <h3 className="slogan-en">Never Stray</h3>
