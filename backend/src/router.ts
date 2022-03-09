3b9bd485cf3e687fa5d2201d3e60bbefb4586275
Tsai-Chieh <d0177381@gmail.com>
2022-03-09 22:08:54 +0800

    :sparkles: add Cache-Control header for all APIs

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,6 +1,6 @@
-
-import {Application} from 'express'
+import {Application, NextFunction, Request, Response} from 'express'
 import {useExpressServer} from 'routing-controllers'
+
 import {EnumController} from './controllers/enum.controller'
 import {PetController} from './controllers/pet.controller'
 import {PingController} from './controllers/ping.controller'
@@ -13,6 +13,10 @@ export class Router {
    * @param  {Application} app
    */
   static router(app: Application): void {
+    app.use((_: Request, res: Response, next: NextFunction) => {
+      res.setHeader('Cache-Control', `public, max-age=${process.env.DAY_SECS}`)
+      next()
+    })
     useExpressServer(app, {
       routePrefix: 'api',
       controllers: [EnumController, PetController, PingController],
