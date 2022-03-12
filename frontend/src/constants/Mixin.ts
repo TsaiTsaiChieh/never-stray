68f6e38a614e6896a0f08901c1da5bd62aebb670
Tsai-Chieh <d0177381@gmail.com>
2022-03-12 16:18:23 +0800

    :tada: init Mixin file for styled component used

---
+++ frontend/src/constants/Mixin.ts
@@ -0,0 +1,6 @@
+const mobile = '568px'
+const table = '569px'
+const desktop = '961px'
+export const MEDIA_MOBILE = `@media all and {max-width: ${mobile}}`
+export const MEDIA_TABLET = `@media all and {min-width: ${table}}`
+export const MEDIA_DESKTOP = `@media all and {min-width: ${desktop}}`
