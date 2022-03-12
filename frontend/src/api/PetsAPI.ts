edc2c2258df96d1895cf84b9fa84f8de80ec76f7
Tsai-Chieh <d0177381@gmail.com>
2022-03-12 16:20:39 +0800

    :construction: search pet API still work in progress

---
+++ frontend/src/api/PetsAPI.ts
@@ -0,0 +1,16 @@
+import axios, {AxiosResponse} from 'axios'
+
+const {REACT_APP_API_URL} = process.env
+
+export async function searchPet(setPets: any) {
+  try {
+    const res: AxiosResponse = await axios({
+      method: 'GET',
+      url: `${REACT_APP_API_URL}/pet?limit=20&page=10`,
+    })
+    const data: PetsAPIType = await res.data
+    setPets(data.pet)
+  } catch (error) {
+    setPets([])
+  }
+}
