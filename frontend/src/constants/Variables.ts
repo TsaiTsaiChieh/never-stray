f4301626b96ac06fb6ab4265b4fbb32f55822076
Tsai-Chieh <d0177381@gmail.com>
2022-03-12 16:32:39 +0800

    :tada: init Variables for styled component used

---
+++ frontend/src/constants/Variables.ts
@@ -0,0 +1,27 @@
+/* eslint-disable max-len */
+export const colors = {
+  tiffany: {
+    i100: '#ebf8f7',
+    i200: '#ebfaf6',
+    i300: '#61ded7',
+    i400: '#00948b',
+  },
+  gray: {
+    i100: '#bdbdbd',
+    i200: '#9fa0a0',
+    i300: '#828282',
+    i400: '#4f4f4f',
+    i500: '#333333',
+    t100: 'rgba(151, 151, 151, 0.3)',
+  },
+}
+
+export const filters = {
+  white: {
+    i100: 'invert(100%) sepia(11%) saturate(7500%) hue-rotate(194deg) brightness(115%) contrast(109%)',
+  },
+  tiffany: {
+    i300: ' invert(93%) sepia(82%) saturate(3562%) hue-rotate(150deg) brightness(91%) contrast(92%)',
+    i400: 'invert(38%) sepia(18%) saturate(4834%) hue-rotate(145deg) brightness(99%) contrast(101%)',
+  },
+}
