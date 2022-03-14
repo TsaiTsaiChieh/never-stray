8f10ad653b9a5a76d156fcc0b792caa8995216e6
Tsai-Chieh <d0177381@gmail.com>
2022-03-14 16:47:22 +0800

    :sparkles: pass more query parameter to searchPet func

--- frontend/src/api/PetsAPI.ts
+++ frontend/src/api/PetsAPI.ts
@@ -1,12 +1,33 @@
 import axios, {AxiosResponse} from 'axios'
+import {Dispatch, SetStateAction} from 'react'
 
 const {REACT_APP_API_URL} = process.env
 
-export async function searchPet(setPets: any) {
+export async function searchPet(
+  setPets: Dispatch<SetStateAction<PetDataType[]>>,
+  filters: SearchPetFilters,
+) {
   try {
+    let url = filters.page ?
+      `${REACT_APP_API_URL}/pets?page=${filters.page}` :
+      `${REACT_APP_API_URL}/pets?page=1`
+    if (filters.limit) url += `&limit=${filters.limit}`
+    if (filters.status) url += `&status=${filters.status}`
+    if (filters.ref) url += `&ref=${filters.ref}`
+    if (filters.city_id) url += `&city_id=${filters.city_id}`
+    if (filters.shelter_id) url += `&shelter_id=${filters.shelter_id}`
+    if (filters.kind) url += `&kind=${filters.kind}`
+    if (filters.sex) url += `&sex=${filters.sex}`
+    if (filters.color) url += `&color=${filters.color}`
+    if (filters.age) url += `&age=${filters.age}`
+    if (filters.region) url += `&region=${filters.region}`
+    if (filters.order_key) {
+      url += `&order_key=${filters.order_key}&ascend=${filters.ascend}`
+    }
+
     const res: AxiosResponse = await axios({
       method: 'GET',
-      url: `${REACT_APP_API_URL}/pets?limit=12&page=1`,
+      url: url,
     })
     const data: PetsAPIType = await res.data
     setPets(data.pet)
