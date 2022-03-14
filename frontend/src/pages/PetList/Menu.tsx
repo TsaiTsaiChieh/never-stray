318d5649b5d131113a5e91ad51852cf3f680495f
Tsai-Chieh <d0177381@gmail.com>
2022-03-14 21:22:53 +0800

    :sparkles: add onClick trigger & display from flex to grid

--- frontend/src/pages/PetList/Menu.tsx
+++ frontend/src/pages/PetList/Menu.tsx
@@ -7,57 +7,48 @@ import {MEDIA_TABLET} from '../../constants/Mixin'
 import {colors, filters, sizes} from '../../constants/Variables'
 
 interface Props {
-  className?: string
-  setPets: Dispatch<SetStateAction<PetDataType[]>>
-  searchFilters: SearchPetFilters
+  className?: string;
+  setPets: Dispatch<SetStateAction<PetDataType[]>>;
+  searchFilters: SearchPetFilters;
 }
 const Menu = ({className = 'Menu', setPets, searchFilters}: Props) => {
   return (
     <div id={className} className={className}>
-      <div className="menu-group">
-        <div className="pet-selection all-pets selected-pets">
-          <div
-            className="selection-wrap all-pets-wrap"
-            onClick={() => searchPet(setPets, searchFilters)}
-          >
-            <img
-              className="pet-selection-svg all-pets-svg "
-              src="images/all-pets.svg"
-              alt="all-pets"
-            />
-            <span className="pet-select-text all-pets-text"></span>
-          </div>
-        </div>
-        <div className="pet-selection cat-pet">
-          <div
-            className="selection-wrap cat-pet-wrap"
-            onClick={() =>
-              searchPet(setPets, {...searchFilters, kind: PetKind.CAT})
-            }
-          >
-            <img
-              className="pet-selection-svg cat-pet-svg"
-              src="images/cat-pet.svg"
-              alt="cat-pet"
-            />
-            <span className="pet-select-text cat-pet-text"></span>
-          </div>
-        </div>
-        <div className="pet-selection dog-pet">
-          <div
-            className="selection-wrap dog-pet-wrap"
-            onClick={() =>
-              searchPet(setPets, {...searchFilters, kind: PetKind.DOG})
-            }
-          >
-            <img
-              className="pet-selection-svg dog-pet-svg"
-              src="images/dog-pet.svg"
-              alt="dog-pet"
-            />
-            <span className="pet-select-text dog-pet-text"></span>
-          </div>
-        </div>
+      <div
+        className="pet-option-wrap all-option option-selected"
+        onClick={() => searchPet(setPets, searchFilters)}
+      >
+        <img
+          className="pet-option-svg all-pets-svg"
+          src="images/Menu/all-option.svg"
+        />
+        <span className="pet-option-text all-pet-text"></span>
+      </div>
+
+      <div
+        className="pet-option-wrap cat-option"
+        onClick={() =>
+          searchPet(setPets, {...searchFilters, kind: PetKind.CAT})
+        }
+      >
+        <img
+          className="pet-option-svg cat-pets-svg"
+          src="images/Menu/cat-option.svg"
+        />
+        <span className="pet-option-text cat-pet-text" />
+      </div>
+
+      <div
+        className="pet-option-wrap dog-option"
+        onClick={() =>
+          searchPet(setPets, {...searchFilters, kind: PetKind.DOG})
+        }
+      >
+        <img
+          className="pet-option-svg dog-pets-svg"
+          src="images/Menu/dog-option.svg"
+        />
+        <span className="pet-option-text dog-pet-text" />
       </div>
     </div>
   )
@@ -66,90 +57,77 @@ const Menu = ({className = 'Menu', setPets, searchFilters}: Props) => {
 const StyledMenu = styled(Menu)`
   width: 100%;
   background-color: ${colors.tiffany.i100};
-  display: flex;
+  padding: ${sizes.m} 0 ${sizes.m} 0;
+  display: grid;
+  grid-template-columns: 3.8rem 3.8rem 3.8rem;
+  grid-column-gap: 0.3rem;
   justify-content: center;
-  padding: 7px 0 7px 0;
   ${MEDIA_TABLET} {
-    padding: 14px 0 14px 0;
+    padding: ${sizes.s} 0 ${sizes.s} 0;
+    grid-template-columns: 6rem 6rem 6rem;
+    grid-column-gap: 1rem;
   }
-
-  .menu-group {
+  .pet-option-wrap {
+    cursor: pointer;
+    text-align: center;
     display: flex;
-    justify-content: space-between;
-
-    .pet-selection {
-      .all-pets-wrap,
-      .cat-pet-wrap {
-        margin-right: 10px;
-        ${MEDIA_TABLET} {
-          margin-right: 1.2rem;
-        }
-        ${MEDIA_TABLET} {
-          margin-right: 3rem;
-        }
+    align-items: center;
+    justify-content: center;
+    .pet-option-svg {
+      display: none;
+      filter: ${filters.tiffany.i400};
+      ${MEDIA_TABLET} {
+        display: block;
+        width: 32%;
+        padding: 4px 8px 4px 8px;
       }
+    }
+    span.pet-option-text {
+      font-size: ${sizes.m};
+      margin: 0 10px 0 10px;
+      padding: 10px 0 10px 0;
+      ${MEDIA_TABLET} {
+        margin-left: 0px;
+      }
+    }
+    span.pet-option-text::first-letter {
+      letter-spacing: 2px;
+    }
+    span.pet-option-text::after {
+      text-align: center;
+      color: ${colors.gray.i500};
+    }
 
-      .selection-wrap {
-        cursor: pointer;
-        display: flex;
-        align-items: center;
+    span.all-pet-text::after {
+      content: "全部";
+    }
 
-        .pet-selection-svg {
-          display: none;
-          filter: ${filters.tiffany.i400};
-          ${MEDIA_TABLET} {
-            display: block;
-            width: 35%;
-            padding: 4px 0px 4px 4px;
-          }
-        }
-
-        span.pet-select-text {
-          font-size: ${sizes.m};
-          margin: 0 10px 0 10px;
-          padding: 10px 0 10px 0;
-        }
-        span.pet-select-text::after {
-          text-align: center;
-          color: ${colors.gray.i500};
-        }
-        span.pet-select-text::first-letter {
-          letter-spacing: 2px;
-        }
-        span.all-pets-text::after {
-          content: "全部";
-        }
-
-        span.cat-pet-text::after {
-          content: "貓";
-          ${MEDIA_TABLET} {
-            content: "喵喵";
-          }
-        }
-
-        span.dog-pet-text::after {
-          content: "狗";
-          ${MEDIA_TABLET} {
-            content: "汪汪";
-          }
-        }
+    span.cat-pet-text::after {
+      content: "貓";
+      ${MEDIA_TABLET} {
+        content: "喵喵";
       }
     }
 
-    /* Selected */
-    .selected-pets {
-      .selection-wrap {
-        background-color: ${colors.tiffany.i300};
-        border-radius: 12px;
-        span.all-pets-text::after,
-        span.cat-pet-text::after,
-        span.dog-pet-text::after {
-          color: ${colors.white.i100};
-        }
-        .pet-selection-svg {
-          filter: ${filters.white.i100};
-        }
+    span.dog-pet-text::after {
+      content: "狗";
+      ${MEDIA_TABLET} {
+        content: "汪汪";
       }
+    }
+  }
+
+  /* Selected */
+  .option-selected {
+    background-color: ${colors.tiffany.i300};
+    border-radius: 12px;
+    span.all-pet-text::after,
+    span.cat-pet-text::after,
+    span.dog-pet-text::after {
+      color: ${colors.white.i100};
+    }
+    .pet-option-svg {
+      filter: ${filters.white.i100};
     }
   }
 `
