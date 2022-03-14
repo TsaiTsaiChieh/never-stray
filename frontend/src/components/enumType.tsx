03fbba5f7c3906d02a5baa83cd86c5e7f82d5a02
Tsai-Chieh <d0177381@gmail.com>
2022-03-14 16:33:31 +0800

    :tada: init enum types

---
+++ frontend/src/components/enumType.tsx
@@ -0,0 +1,37 @@
+export enum Ternary {
+  TRUE = 'T',
+  FALSE = 'F',
+  UNKNOWN = 'U',
+}
+
+export enum PetRef {
+  GOV = 'gov',
+  MAP = 'map',
+  OWN = 'own',
+}
+
+export enum PetSex {
+  FEMALE = 'F',
+  MALE = 'M',
+  UNKNOWN = 'U',
+}
+
+export enum PetAge {
+  ADULT = 'A',
+  CHILD = 'C',
+  UNKNOWN = 'U',
+}
+
+export enum PetStatus {
+  UNKNOWN = 'unknown',
+  OPEN = 'open',
+  ADOPTED = 'adopted',
+  OTHER = 'other',
+  DEAD = 'dead',
+}
+
+export enum PetKind {
+  DOG = 'dog',
+  CAT = 'cat',
+  OTHER = 'other',
+}
