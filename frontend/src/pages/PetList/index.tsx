576d9f317b5ba460c017ca9d28285b70ba081e8c
Tsai-Chieh <d0177381@gmail.com>
2022-03-17 22:52:24 +0800

    :sparkles: add total state & Pagination component

--- frontend/src/pages/PetList/index.tsx
+++ frontend/src/pages/PetList/index.tsx
@@ -2,35 +2,47 @@ import {ReactElement, useEffect, useState} from 'react'
 
 import {searchPet} from '../../api/PetsAPI'
 import StyledFooter from '../../components/Footer'
+import StyledPagination from '../../components/Pagination'
 import {PetStatus} from '../../constants/EnumType'
 import StyledBanner from './Banner'
 import StyledMenu from './Menu'
 import StyledPetProfile from './PetProfile'
 
 export default function PetList(): ReactElement {
+  const [_, setTotal] = useState<number>(0)
   const [pets, setPets] = useState<PetDataType[]>([])
-  const [searchFilters, _] = useState<SearchPetFilters>({
+  const [searchFilters, setSearchFilters] = useState<SearchPetFilters>({
     status: PetStatus.OPEN,
     limit: 12,
     page: 1,
     ascend: true,
   })
-
   useEffect(() => {
-    searchPet(setPets, searchFilters)
+    searchPet(setPets, setTotal, searchFilters)
   }, [])
 
   return (
     <>
-      <StyledMenu setPets={setPets} searchFilters={searchFilters} />
+      <StyledMenu
+        setPets={setPets}
+        setTotal={setTotal}
+        setSearchFilters={setSearchFilters}
+        searchFilters={searchFilters}
+      />
       <StyledBanner />
-      <div id="PetList" >
+      <div id="PetList">
         <>
           {pets.map((ele) => (
             <StyledPetProfile key={ele.id} pet={ele} />
           ))}
         </>
       </div>
+      <StyledPagination
+        setPets={setPets}
+        setTotal={setTotal}
+        setSearchFilters={setSearchFilters}
+        searchFilters={searchFilters}
+      />
       <StyledFooter />
     </>
   )
