a41668d4de0e412d8e62feb9b4891c965291abe0
Tsai-Chieh <d0177381@gmail.com>
2022-03-17 11:24:11 +0800

    :sparkles: add Footer

--- frontend/src/pages/PetList/index.tsx
+++ frontend/src/pages/PetList/index.tsx
@@ -1,6 +1,7 @@
 import {ReactElement, useEffect, useState} from 'react'
 
 import {searchPet} from '../../api/PetsAPI'
+import StyledFooter from '../../components/Footer'
 import {PetStatus} from '../../constants/EnumType'
 import StyledBanner from './Banner'
 import StyledMenu from './Menu'
@@ -30,6 +31,7 @@ export default function PetList(): ReactElement {
           ))}
         </>
       </div>
+      <StyledFooter />
     </>
   )
 }
