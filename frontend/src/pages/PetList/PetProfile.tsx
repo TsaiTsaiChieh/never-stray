84883c704d6c8288323a8d78ffe9fc4b57d6c743
Tsai-Chieh <d0177381@gmail.com>
2022-03-18 22:19:46 +0800

    :recycle: refine the styled component

--- frontend/src/pages/PetList/PetProfile.tsx
+++ frontend/src/pages/PetList/PetProfile.tsx
@@ -1,7 +1,15 @@
-import styled from 'styled-components'
-import {MEDIA_TABLET} from '../../constants/Mixin'
-
-import {colors, filters, sizes} from '../../constants/Variables'
+import {
+  Age,
+  Container,
+  ImgWrap,
+  PawImg,
+  PetImg,
+  ProfileWrap,
+  Sex,
+  SexAgeWrap,
+  Title,
+  VerticalLine,
+} from '../../styled/PetList/PetProfile'
 import {
   petAgeConverter,
   petKindConverter,
@@ -9,171 +17,35 @@ import {
 } from '../../utils/value-convert'
 
 interface Props {
-  className?: string
-  pet: PetDataType
+  pet: PetDataType;
 }
-const PetProfile = ({className, pet}: Props) => {
+const PetProfile = ({pet}: Props) => {
   return (
-    <div id='PetProfile' className={className}>
-      <div className="pet-container">
-        <div className="pet-image-wrapper">
-          <img className="pet-paw" src="/images/PetList/paw.svg" />
-          <div className="pet-image"
+    <ProfileWrap>
+      <Container>
+        <ImgWrap>
+          <PawImg src="/images/PetList/paw.svg" />
+          <PetImg
+            image={pet.image[0]}
             style={{
-              backgroundImage: `url(${pet.image[0] ? pet.image[0] :
-                '/images/PetList/pet-mask.svg'})`,
+              backgroundImage: `url(${pet.image[0] ?
+                pet.image[0] : '/images/PetList/pet-mask.svg'})`,
             }} />
-          <h3 className="pet-title">
+          <Title>
             {pet.color}
             {petKindConverter(pet.kind)}
-          </h3>
-          <div className="pet-sex-age-wrapper">
-            <p className="pet-sex">{petSexConverter(pet.sex)}</p>
-            <span className="pet-vl" />
-            <p className="pet-age">{petAgeConverter(pet.age)}</p>
-          </div>
-        </div>
-      </div>
-    </div>
+          </Title>
+          <SexAgeWrap>
+            <Sex className="pet-sex" sex={pet.sex}>
+              {petSexConverter(pet.sex)}
+            </Sex>
+            <VerticalLine className="pet-vl" />
+            <Age className="pet-age">{petAgeConverter(pet.age)}</Age>
+          </SexAgeWrap>
+        </ImgWrap>
+      </Container>
+    </ProfileWrap>
   )
 }
-const StyledPetProfile = styled(PetProfile)`
-  display: flex;
-  text-align: center;
-  justify-content: center;
-  box-shadow: 0px 0px 8px ${colors.gray.t100};
-  border-radius: 5px;
 
-  .pet-container {
-    display: flex;
-    height: 11rem;
-    flex-direction: column;
-    padding-left: 1.2rem;
-    padding-right: 1.2rem;
-    ${MEDIA_TABLET} {
-      height: 11.6rem;
-      padding: 1.9rem;
-    }
-
-    .pet-image-wrapper {
-      position: relative;
-      bottom: 8rem;
-      ${MEDIA_TABLET} {
-        bottom: 10.2rem;
-      }
-      .pet-paw {
-        filter: ${filters.tiffany.i300};
-        position: relative;
-        top: 2.5rem;
-        right: 0.6rem;
-        ${MEDIA_TABLET} {
-          top: 2.7rem;
-          width: 90%;
-        }
-      }
-      .pet-image {
-        cursor: pointer;
-        position: relative;
-        object-fit: cover;
-        width: 9rem;
-        height: 8.5rem;
-        border-radius: 10px;
-        filter: ${(props) => props.pet.image[0] ? '' : filters.blue.i100};
-        background-repeat: no-repeat;
-        background-position: center;
-        background-size: cover;
-        -webkit-mask: url("/images/PetList/pet-mask.svg") no-repeat center
-          center;
-        mask: url("/images/PetList/pet-mask.svg") no-repeat center center;
-        -webkit-mask-size: contain;
-        mask-size: contain;
-        ${MEDIA_TABLET} {
-          width: 10rem;
-          height: 9.5rem;
-        }
-      }
-      .pet-image:hover {
-        -webkit-filter: brightness(65%);
-        transition: all 0.8s ease;
-      }
-    }
-
-    .pet-title {
-      position: relative;
-      top: 0.8rem;
-      font-size: ${sizes.s};
-      ${MEDIA_TABLET} {
-        font-size: ${sizes.m};
-        top: 1rem;
-      }
-    }
-
-    .pet-title:before {
-      content: "";
-      position: absolute;
-      top: 1.6rem;
-      right: 3.4rem;
-      width: 33%;
-      border-bottom: 3px solid ${colors.tiffany.i300};
-    }
-
-    .pet-sex-age-wrapper {
-      position: relative;
-      letter-spacing: 1px;
-      top: 2.5rem;
-      display: flex;
-      flex-direction: row;
-      cursor: pointer;
-      color: ${colors.gray.i300};
-      font-size: ${sizes.m};
-      justify-content: space-around;
-      padding: 0.5rem;
-      border-radius: 10px;
-      text-align: center;
-      background: ${colors.tiffany.i200};
-      align-items: center;
-      ${MEDIA_TABLET} {
-        .pet-age {
-          margin-left: -3rem;
-        }
-        top: 4.6rem;
-        padding-top: 1.1rem;
-        padding-bottom: 1.1rem;
-        margin-left: -0.95rem;
-        margin-right: -0.95rem;
-        .pet-vl::before {
-          content: "｜";
-          margin-left: -3rem;
-          color: ${colors.gray.i600};
-        }
-      }
-      .pet-sex::before {
-        content: "";
-        margin-right: 5px;
-        background: ${(props) =>
-    `url(/images/PetList/pet-sex-${props.pet.sex}.svg)`};
-        background-repeat: no-repeat;
-        width: ${sizes.m};
-        height: ${sizes.m};
-        display: block;
-        float: left;
-      }
-    }
-    .pet-sex-age-wrapper:hover {
-      background: ${colors.tiffany.i500};
-      color: ${colors.white.i100};
-      transition: all 0.8s ease;
-      .pet-sex,
-      .pet-age {
-        display: none;
-      }
-      .pet-vl::before {
-        margin-left: 0;
-        content: "瞭解我多一點";
-        color: ${colors.tiffany.i400};
-        letter-spacing: 2px;
-      }
-    }
-  }
-`
-export default StyledPetProfile
+export default PetProfile
---
+++ frontend/src/styled/PetList/PetProfile.tsx
@@ -0,0 +1,144 @@
+import styled from 'styled-components'
+
+import {MEDIA_TABLET} from '../../constants/Mixin'
+import {colors, filters, sizes} from '../../constants/Variables'
+
+export const ProfileWrap = styled.div`
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  box-shadow: 0px 0px 8px ${colors.gray.t100};
+  border-radius: 5px;
+`
+export const Container = styled.div`
+  display: flex;
+  height: 11rem;
+  flex-direction: column;
+  padding-left: 1.2rem;
+  padding-right: 1.2rem;
+  ${MEDIA_TABLET} {
+    height: 11.6rem;
+    padding: 1.9rem;
+  }
+`
+export const ImgWrap = styled.div`
+  position: relative;
+  bottom: 8rem;
+  ${MEDIA_TABLET} {
+    bottom: 10.2rem;
+  }
+`
+export const PawImg = styled.img`
+  filter: ${filters.tiffany.i300};
+  position: relative;
+  top: 2.5rem;
+  right: 0.6rem;
+  ${MEDIA_TABLET} {
+    top: 2.7rem;
+    width: 90%;
+  }
+`
+export const PetImg = styled.div<{image: string}>`
+  cursor: pointer;
+  position: relative;
+  object-fit: cover;
+  width: 9rem;
+  height: 8.5rem;
+  border-radius: 10px;
+  filter: ${(props) => (props.image ? '' : filters.tiffany.i300)};
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  -webkit-mask: url("/images/PetList/pet-mask.svg") no-repeat center center;
+  mask: url("/images/PetList/pet-mask.svg") no-repeat center center;
+  -webkit-mask-size: contain;
+  mask-size: contain;
+  ${MEDIA_TABLET} {
+    width: 10rem;
+    height: 9.5rem;
+  }
+  &:hover {
+    -webkit-filter: brightness(65%);
+    transition: all 0.8s ease;
+  }
+`
+export const Title = styled.h3`
+  position: relative;
+  top: 0.8rem;
+  font-size: ${sizes.s};
+  ${MEDIA_TABLET} {
+    font-size: ${sizes.m};
+    top: 1rem;
+  }
+  &:before {
+    content: "";
+    position: absolute;
+    top: 1.6rem;
+    right: 3.4rem;
+    width: 33%;
+    border-bottom: 3px solid ${colors.tiffany.i300};
+  }
+`
+export const SexAgeWrap = styled.div`
+  position: relative;
+  letter-spacing: 1px;
+  top: 2.5rem;
+  display: flex;
+  flex-direction: row;
+  cursor: pointer;
+  color: ${colors.gray.i300};
+  font-size: ${sizes.m};
+  justify-content: space-around;
+  padding: 0.5rem;
+  border-radius: 10px;
+  text-align: center;
+  background: ${colors.tiffany.i200};
+  align-items: center;
+  ${MEDIA_TABLET} {
+    top: 4.6rem;
+    padding-top: 1.1rem;
+    padding-bottom: 1.1rem;
+    margin-left: -0.95rem;
+    margin-right: -0.95rem;
+  }
+  &:hover {
+    background: ${colors.tiffany.i500};
+    color: ${colors.white.i100};
+    transition: all 0.8s ease;
+    .pet-sex,
+    .pet-age {
+      display: none;
+    }
+    .pet-vl::before {
+      margin-left: 0;
+      content: "瞭解我多一點";
+      color: ${colors.tiffany.i400};
+      letter-spacing: 2px;
+    }
+  }
+`
+export const Sex = styled.p<{sex: PetSexType}>`
+  &::before {
+    content: "";
+    margin-right: 5px;
+    background: ${(props) => `url(/images/PetList/pet-sex-${props.sex}.svg)`};
+    background-repeat: no-repeat;
+    width: ${sizes.m};
+    height: ${sizes.m};
+    display: block;
+    float: left;
+  }
+`
+export const VerticalLine = styled.span`
+  &::before {
+    content: "｜";
+    margin-left: -3rem;
+    color: ${colors.gray.i600};
+  }
+`
+export const Age = styled.p`
+  ${MEDIA_TABLET} {
+    margin-left: -3rem;
+  }
+`
+
