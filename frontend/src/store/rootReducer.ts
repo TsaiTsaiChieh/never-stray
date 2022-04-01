40a0aaa386fdfa1606bcf2ec0fbc403ff391f21d
Tsai-Chieh <d0177381@gmail.com>
2022-04-01 13:58:18 +0800

    :sparkles: add enum reducer

--- frontend/src/store/rootReducer.ts
+++ frontend/src/store/rootReducer.ts
@@ -1,8 +1,11 @@
 import {combineReducers} from 'redux'
+
+import enumReducer from './reducers/enumSlice'
 import petListReducer from './reducers/petListSlice'
 
 const rootReducer = combineReducers({
   petList: petListReducer,
+  enum: enumReducer,
 })
 
 export default rootReducer
