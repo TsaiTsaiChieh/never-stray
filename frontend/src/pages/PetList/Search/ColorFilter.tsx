c90cadd8e11b7f2ecc3f0744e19a25a7103e25df
Tsai-Chieh <d0177381@gmail.com>
2022-04-02 14:19:16 +0800

    :tada: init ColorFilter component

---
+++ frontend/src/pages/PetList/Search/ColorFilter.tsx
@@ -0,0 +1,51 @@
+import makeAnimated from 'react-select/animated'
+
+import {useAppDispatch, useAppSelector} from '../../../store/hooks'
+import {getPets} from '../../../store/reducers/petListSlice'
+import {
+  ColorName,
+  ColorSelector,
+  ColorWrap,
+} from '../../../styled/PetList/SearchBoard'
+
+const ColorFilter = () => {
+  const {filters} = useAppSelector((state) => state.petList)
+  const {colors} = useAppSelector((state) => state.enum)
+  const dispatch = useAppDispatch()
+  const animatedComponents = makeAnimated()
+  const options: OptionsType[] = colors.map((ele) =>
+    ele ?
+      {
+        value: ele,
+        label: ele,
+      } :
+      {value: '', label: '未填'},
+  )
+
+  const onChange = (newValue: any) => {
+    dispatch(
+      getPets({
+        ...filters,
+        color: newValue.map((ele: OptionsType) => ele.value),
+      }),
+    )
+  }
+
+  return (
+    <ColorWrap className="ColorWrap">
+      <ColorName>顏色</ColorName>
+      <ColorSelector
+        isMulti
+        classNamePrefix="Select"
+        closeMenuOnSelect={false}
+        components={animatedComponents}
+        options={options}
+        onChange={onChange}
+        placeholder="請選擇顏色"
+        noOptionsMessage={() => '無'}
+      />
+    </ColorWrap>
+  )
+}
+
+export default ColorFilter
