9e1141f28fa03e5bb04ccfbc3ddc5942d4957e0a
Tsai-Chieh <d0177381@gmail.com>
2022-04-02 14:17:56 +0800

    :lipstick: add Selector & fine-tuning

--- frontend/src/styled/PetList/SearchBoard.tsx
+++ frontend/src/styled/PetList/SearchBoard.tsx
@@ -1,3 +1,4 @@
+import Select from 'react-select'
 import styled from 'styled-components'
 
 import {MEDIA_DESKTOP, MEDIA_TABLET} from '../../constants/Mixin'
@@ -26,6 +27,7 @@ const Items = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, 80px);
   grid-column-gap: 0.5rem;
+  justify-content: space-between;
   ${MEDIA_TABLET} {
     grid-column-gap: 0.6rem;
   }
@@ -35,6 +37,7 @@ const Wrap = styled.div`
   flex-direction: column;
   position: relative;
   width: 90%;
+  padding-bottom: 22px;
 `
 const Name = styled.h3`
   font-size: ${sizes.xs};
@@ -95,6 +98,34 @@ const Content = styled.p`
   letter-spacing: 1px;
   font-size: ${sizes.xs};
 `
+const Selector = styled(Select)`
+  font-size: ${sizes.xs};
+  letter-spacing: 1px;
+  .Select__control:hover {
+    border-color: ${colors.tiffany.i301};
+  }
+  .Select__control--menu-is-open {
+    border-color: ${colors.tiffany.i300};
+  }
+  .Select__control--is-focused {
+    box-shadow: 0 0 0 1px ${colors.tiffany.i300};
+    outline: none;
+  }
+  .Select__option {
+    background: white;
+    color: ${colors.gray.i400};
+  }
+  .Select__option:hover {
+    background: ${colors.tiffany.i100};
+  }
+  .Select__multi-value {
+    background: ${colors.tiffany.i100};
+  }
+  /* .Select__dropdown-indicator {
+    background: url("/images/down-arrow.png");
+    background-repeat: no-repeat;
+  } */
+`
 export const Closed = styled(CloseButton)`
   ${MEDIA_DESKTOP} {
     display: none;
@@ -103,21 +134,23 @@ export const Closed = styled(CloseButton)`
 export const SearchBoardContainer = styled.div`
   display: none;
   width: 80%;
-  height: 65vh;
+  height: 70vh;
   position: fixed;
-  justify-items: center;
   align-content: center;
   grid-template-rows: repeat(auto-fill, 80px);
   background: white;
   z-index: 10;
+  overflow-y: auto;
+  grid-auto-flow: column;
   margin: auto;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
   border-radius: 10px;
-  padding-top: 20px;
+  padding-top: 50px;
   padding-bottom: 20px;
+  padding-left: 30px;
   ${MEDIA_TABLET} {
     display: none;
     position: absolute;
@@ -129,7 +162,7 @@ export const SearchBoardContainer = styled.div`
     width: 100%;
     min-width: 367px;
     padding-left: ${sizes.xl};
-    grid-template-rows: repeat(auto-fill, 90px);
+    grid-template-rows: repeat(auto-fill, minmax(90px, auto));
     box-shadow: 8px 0 12px ${colors.gray.t100};
   }
   ${MEDIA_DESKTOP} {
@@ -138,9 +171,10 @@ export const SearchBoardContainer = styled.div`
     height: 100%;
     min-height: 700px;
     max-height: 700px;
+    align-content: space-between;
     width: 100%;
-    min-width: 310px;
-    max-width: 310px;
+    min-width: 290px;
+    max-width: 290px;
     box-shadow: 0 0 8px ${colors.gray.t100};
     border-radius: 5px;
     top: 40px;
@@ -149,7 +183,6 @@ export const SearchBoardContainer = styled.div`
     right: 0;
   }
 `
-
 export const AgeWrap = styled(Wrap)``
 export const AgeName = styled(Name)``
 export const AgeItems = styled(Items)``
@@ -160,13 +193,12 @@ export const SexName = styled(Name)``
 export const SexItems = styled(Items)``
 export const SexItem = styled(Button)``
 
-export const LigationWrap = styled(Wrap)`
-  /* background: pink; */
-`
+export const LigationWrap = styled(Wrap)``
 export const LigationName = styled(Name)``
 export const LigationItems = styled(Items)``
 export const LigationItem = styled(Button)``
 
+export const ColorSelector = styled(Selector)``
 export const ColorWrap = styled(Wrap)``
 export const ColorDropDownContent = styled(DropDownContent)``
 export const ColorDropDown = styled(DropDown)`
