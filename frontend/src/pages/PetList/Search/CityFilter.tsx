71f5ed6c4cafe095fce5bd6a977b12a5f062691c
Tsai-Chieh <d0177381@gmail.com>
2022-04-02 20:11:47 +0800

    :tada: init CityFilter component

---
+++ frontend/src/pages/PetList/Search/CityFilter.tsx
@@ -0,0 +1,43 @@
+import {useAppDispatch, useAppSelector} from '../../../store/hooks'
+import {getPets} from '../../../store/reducers/petListSlice'
+import {
+  CityName,
+  CitySelector,
+  CityWrap,
+} from '../../../styled/PetList/SearchBoard'
+
+const CityFilter = () => {
+  const {filters} = useAppSelector((state) => state.petList)
+  const {cities} = useAppSelector((state) => state.enum)
+  const dispatch = useAppDispatch()
+  const options: OptionType[] = cities.map((ele) => ({
+    value: ele.id.toString(),
+    label: ele.name,
+  }))
+
+  const onChange = (newValue: any) => {
+    dispatch(
+      getPets({
+        ...filters,
+        city_id: newValue.map((ele: OptionType) => parseInt(ele.value)),
+        page: 1,
+      }),
+    )
+  }
+  return (
+    <CityWrap>
+      <CityName>縣市</CityName>
+      <CitySelector
+        isMulti
+        classNamePrefix="Select"
+        closeMenuOnSelect={false}
+        options={options}
+        onChange={onChange}
+        placeholder="請選擇顏色"
+        noOptionsMessage={() => '無'}
+      />
+    </CityWrap>
+  )
+}
+
+export default CityFilter
