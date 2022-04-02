355a274dd419d2639384ec6c34f0ec4fb90b7b39
Tsai-Chieh <d0177381@gmail.com>
2022-04-02 18:17:03 +0800

    :sparkles: add puppeteer helper funcs

--- backend/src/utils/helper.ts
+++ backend/src/utils/helper.ts
@@ -1,3 +1,5 @@
+import {Page} from 'puppeteer'
+
 /**
  * Deep copy
  *
@@ -6,4 +8,60 @@
  */
 export function deepCopy(data: any): any {
   return JSON.parse(JSON.stringify(data))
+}
+
+/**
+ * Get single HTML element value
+ *
+ * @param  {Page} page the page from puppeteer
+ * @param  {string} eleName element name
+ * @return {Promise<string | null>}
+ */
+export async function getElementValue(
+  page: Page,
+  eleName: string,
+): Promise<string | null> {
+  return await page.$eval(eleName, (el) => el.textContent)
+}
+
+/**
+ * Get all HTML element values
+ *
+ * @param  {Page} page the page from puppeteer
+ * @param  {string} eleName element name
+ * @return {Promise<(string | null)[]>}
+ */
+export async function getElementsValues(page: Page,
+  eleName: string): Promise<(string | null)[]> {
+  return await page.$$eval(eleName, (els) => els.map((el) => el.textContent))
+}
+
+/**
+ * Get single HTML href value
+ *
+ * @param  {Page} page the page from puppeteer
+ * @param  {string} eleName element name
+ * @return {Promise<string | null>}
+ */
+export async function getLink(
+  page: Page,
+  eleName: string,
+): Promise<string | null> {
+  return await page.$eval(eleName, (ele) => ele.getAttribute('href'))
+}
+
+/**
+ * Get all HTML href values
+ *
+ * @param  {Page} page the page from puppeteer
+ * @param  {string} eleName element name
+ * @return {Promise<(string | null)[]> }
+ */
+export async function getLinks(
+  page: Page,
+  eleName: string,
+): Promise<(string | null)[]> {
+  return await page.$$eval(eleName, (els) =>
+    els.map((el) => el.getAttribute('href')),
+  )
 }
