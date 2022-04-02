d6db0a2d65498a6e93b6ef046f84d79576d4861d
Tsai-Chieh <d0177381@gmail.com>
2022-04-02 20:06:06 +0800

    :sparkles: add getCities API, type annotation warning ref from https://stackoverflow.com/questions/70112204/type-unknown-is-not-assignable-to-type-writabledraft-writabledraftuser

--- frontend/src/store/reducers/enumSlice.ts
+++ frontend/src/store/reducers/enumSlice.ts
@@ -1,12 +1,14 @@
 import axios, {AxiosResponse} from 'axios'
 
-import {createAsyncThunk, createSlice} from '@reduxjs/toolkit'
-import {RootState} from '..'
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit'
+
+import {RootState} from '../'
 
 const {REACT_APP_API_URL} = process.env
 
 const initialState: EnumState = {
   colors: [],
+  cities: [],
 }
 
 export const getPetColors = createAsyncThunk(
@@ -18,14 +20,34 @@ export const getPetColors = createAsyncThunk(
   },
 )
 
+export const getCities = createAsyncThunk(
+  'petList/fetchCities',
+  async () => {
+    const url = `${REACT_APP_API_URL}/enum/city`
+    const res: AxiosResponse = await axios({method: 'GET', url})
+    const data: CityAPIType[] = res.data
+    return data
+  },
+)
+
 export const enumSlice = createSlice({
   name: 'enum',
   initialState,
   reducers: {},
-  extraReducers: (builder) =>
+  extraReducers: (builder) => {
     builder.addCase(getPetColors.fulfilled, (state, action) => {
       state.colors = action.payload
-    }),
+    })
+    builder.addCase(
+      getCities.fulfilled,
+      (state, action: PayloadAction<CityAPIType[]>) => {
+        state.cities = action.payload.map((ele: CityAPIType) => ({
+          id: ele.id,
+          name: ele.name,
+        }))
+      },
+    )
+  },
 })
 
 export const selectEnum = (state: RootState) => state.enum
