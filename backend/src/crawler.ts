cae23808f353be20fe9497d79641c102679195a4
Tsai-Chieh <d0177381@gmail.com>
2022-04-07 08:57:53 +0800

    :alembic: try to run getMeetPetData scheduler at 9:05 (UTU+8)

--- backend/src/crawler.ts
+++ backend/src/crawler.ts
@@ -22,7 +22,7 @@ const JOB_PORT: number = parseInt(process.env.JOB_PORT!);
     )
     scheduleJob(
       'Get meet pet website data scheduler',
-      '50 1 * * *',
+      '5 1 * * *',
       async (): Promise<void> => {
         await getMeetPetData()
       },
