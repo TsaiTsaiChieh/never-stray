232d2a8578fae1de3b2127be15f98c09c7215980
Tsai-Chieh <d0177381@gmail.com>
2022-04-07 22:02:21 +0800

    :alembic: just run getMeetPetData quickly

--- backend/src/crawler.ts
+++ backend/src/crawler.ts
@@ -15,14 +15,14 @@ const JOB_PORT: number = parseInt(process.env.JOB_PORT!);
     console.info(`[${env}] Crawler listening on ${JOB_PORT}`)
     scheduleJob(
       'Get shelter data scheduler',
-      '0 */1 * * *',
+      '* */1 * * *',
       async (): Promise<void> => {
         await getShelterData()
       },
     )
     scheduleJob(
       'Get meet pet website data scheduler',
-      '57 */1 * * *',
+      '*/4 * * * *',
       async (): Promise<void> => {
         await getMeetPetData()
       },
