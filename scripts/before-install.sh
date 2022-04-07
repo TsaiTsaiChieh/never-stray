ea8314863f85c2483142bd946bad7a19979ce300
Tsai-Chieh <d0177381@gmail.com>
2022-04-07 23:53:59 +0800

    Revert ":recycle: referring to other components by  name, and rm className, ref from https://styled-components.com/docs/advanced#referring-to-other-components"
    This reverts commit dfd46fcb850635e557034a1dd758cdc3834caf20.

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,8 +1,8 @@
 echo "Before install"
 cd /home/ubuntu/never-stray
 git checkout main
-git fetch --all
-git reset --hard origin/main
+git reset --hard HEAD
+git pull
 echo "Docker clean"
 docker container prune -f
 docker image prune -f
