1d527012e5737864e6c1a9b4bf09efe84bf11e7d
Tsai-Chieh <d0177381@gmail.com>
2022-04-08 00:04:52 +0800

    :alembic: try to force 'git pull' to overwrite local files, ref from https://stackoverflow.com/questions/1125968/how-do-i-force-git-pull-to-overwrite-local-files

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,8 +1,11 @@
 echo "Before install"
 cd /home/ubuntu/never-stray
 git checkout main
-git reset --hard HEAD
 git pull
+git add *
+git commit -a -m "local file server commit message"
+git fetch origin master
+git merge -s recursive -X theirs origin/main
 echo "Docker clean"
 docker container prune -f
 docker image prune -f
