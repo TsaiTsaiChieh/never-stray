103f11148042a4727957e74440776ba04f1da76b
Tsai-Chieh <d0177381@gmail.com>
2022-04-07 21:51:29 +0800

    :fire: rm memory limit when docker build, ref from https://www.baeldung.com/ops/docker-memory-limit

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,6 +1,6 @@
 echo "After install"
 cd /home/ubuntu/never-stray
-docker build --cache-from=frontend -m 500M -t 'frontend' ./frontend
-docker build --cache-from=backend -m 500M -t 'backend' ./backend
-docker build --cache-from=backend -m 500M -t 'cron' ./backend
-docker build --cache-from=nginx -m 500M -t 'nginx' ./nginx
+docker build --cache-from=frontend-t 'frontend' ./frontend
+docker build --cache-from=backend -t 'backend' ./backend
+docker build --cache-from=backend -t 'cron' ./backend
+docker build --cache-from=nginx -t 'nginx' ./nginx
