1c09aebb14642122b3626e3a62ab542d5aa1a90c
Tsai-Chieh <d0177381@gmail.com>
2022-04-09 01:42:07 +0800

    :iphone: tune FilterButton styled-component for phone

--- frontend/src/styled/PetList/FilterButton.ts
+++ frontend/src/styled/PetList/FilterButton.ts
@@ -1,19 +1,32 @@
 import styled from 'styled-components'
 
-import {MEDIA_DESKTOP} from '../../constants/Mixin'
+import {MEDIA_DESKTOP, MEDIA_TABLET} from '../../constants/Mixin'
 import {colors, filters, sizes} from '../../constants/Variables'
 
 export const FilterContainer = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 16px;
+  left: 16px;
+  cursor: pointer;
+  ${MEDIA_TABLET} {
+    top: 26px;
+    left: 26px;
+  }
   ${MEDIA_DESKTOP} {
     position: absolute;
     top: 30px;
     left: 20px;
+    cursor: auto;
   }
 `
 export const Name = styled.div`
   margin-left: 7px;
   font-size: ${sizes.s};
+  color: ${colors.tiffany.i300};
+  letter-spacing: 1px;
   &::after {
     content: "篩選";
   }
@@ -25,7 +38,8 @@ export const Icon = styled.img`
   width: 18px;
   height: 16px;
   content: url("/images/filter.svg");
+  filter: ${filters.tiffany.i300};
   ${MEDIA_DESKTOP} {
-    filter: ${filters.white.i200};
+    filter: ${filters.gray.i100};
   }
 `
