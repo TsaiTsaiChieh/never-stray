dcf8ca02710934aa86014203594ae5c6a25f8c30
Tsai-Chieh <d0177381@gmail.com>
2022-04-08 22:35:21 +0800

    :tada: init FilterButton component

---
+++ frontend/src/pages/PetList/FilterButton.tsx
@@ -0,0 +1,10 @@
+import {FilterContainer, Icon, Name} from '../../styled/PetList/FilterButton'
+
+export const FilterButton = () => {
+  return (
+    <FilterContainer>
+      <Icon />
+      <Name />
+    </FilterContainer>
+  )
+}
