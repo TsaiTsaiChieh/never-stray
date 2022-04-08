ab14fbc5c7c2314af6f31f2a8aa854617801ed24
Tsai-Chieh <d0177381@gmail.com>
2022-04-09 01:38:53 +0800

    :lipstick: add more wrap for adding FilterButton & KindContainer component

--- frontend/src/pages/PetList/Menu.tsx
+++ frontend/src/pages/PetList/Menu.tsx
@@ -1,7 +1,14 @@
 import {PetKind} from '../../constants/EnumType'
 import {useAppDispatch, useAppSelector} from '../../store/hooks'
 import {getPets} from '../../store/reducers/petListSlice'
-import {Img, StyledMenu, Text, Wrap} from '../../styled/PetList/Menu'
+import {
+  Img,
+  KindContainer,
+  StyledMenu,
+  Text,
+  Wrap,
+} from '../../styled/PetList/Menu'
+import {FilterButton} from './FilterButton'
 
 const Menu = () => {
   const state = useAppSelector((state) => state.petList)
@@ -9,20 +16,25 @@ const Menu = () => {
 
   return (
     <StyledMenu>
-      {Object.values(PetKind).map((kind) => (
-        <Wrap
-          key={kind}
-          className={`${kind}-option
+      <FilterButton />
+      <KindContainer >
+        {Object.values(PetKind).map((kind) => (
+          <Wrap
+            key={kind}
+            className={`${kind}-option
           ${state.filters.kind === kind ? 'option-selected' : ''}`}
-          onClick={() => {
-            dispatch(getPets({...state.filters, kind, page: 1}))
-          }}>
-          <Img
-            className={`${kind}-svg`}
-            src={`images/Menu/${kind}-option.svg`} />
-          <Text className={`${kind}-text`} />
-        </Wrap>
-      ))}
+            onClick={() => {
+              dispatch(getPets({...state.filters, kind, page: 1}))
+            }}
+          >
+            <Img
+              className={`${kind}-svg`}
+              src={`images/Menu/${kind}-option.svg`}
+            />
+            <Text className={`${kind}-text`} />
+          </Wrap>
+        ))}
+      </KindContainer>
     </StyledMenu>
   )
 }
