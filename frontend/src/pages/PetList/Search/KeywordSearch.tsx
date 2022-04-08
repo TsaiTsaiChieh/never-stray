d8ca11b076676a59dd5849b6c314c72e440c3f4c
Tsai-Chieh <d0177381@gmail.com>
2022-04-09 01:33:06 +0800

    :tada: init KeywordSearch component

---
+++ frontend/src/pages/PetList/Search/KeywordSearch.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const StyledKeywordSearch = styled.img`
+content: url('/images/Menu/keyword-search.svg');
+`
+const KeywordSearch = () => {
+  return (
+    <StyledKeywordSearch />
+  )
+}
+
+export default KeywordSearch
