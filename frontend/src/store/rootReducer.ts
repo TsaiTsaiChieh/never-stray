4882b2864a1519a0b755fa9e51496cfa0f025b00
Tsai-Chieh <d0177381@gmail.com>
2022-04-09 16:19:17 +0800

    :sparkles: add uiReducer

--- frontend/src/store/rootReducer.ts
+++ frontend/src/store/rootReducer.ts
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux'
 
 import enumReducer from './reducers/enumSlice'
 import petListReducer from './reducers/petListSlice'
+import uiReducer from './reducers/uiSlice'
 
 const rootReducer = combineReducers({
   petList: petListReducer,
   enum: enumReducer,
+  ui: uiReducer,
 })
 
 export default rootReducer
