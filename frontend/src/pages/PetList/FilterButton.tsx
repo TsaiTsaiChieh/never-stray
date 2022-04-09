afd7c9363d15290654f8c5d50cd190daf90fe2c8
Tsai-Chieh <d0177381@gmail.com>
2022-04-09 16:18:55 +0800

    :sparkles: add onClick action

--- frontend/src/pages/PetList/FilterButton.tsx
+++ frontend/src/pages/PetList/FilterButton.tsx
@@ -1,8 +1,19 @@
+import {isDesktop} from 'react-device-detect'
+
+import {useAppDispatch} from '../../store/hooks'
+import {filterBtnOnClick} from '../../store/reducers/uiSlice'
 import {FilterContainer, Icon, Name} from '../../styled/PetList/FilterButton'
 
 export const FilterButton = () => {
+  const useDispatch = useAppDispatch()
+
+  const clickHandler = () => {
+    if (isDesktop) return
+    useDispatch(filterBtnOnClick())
+  }
+
   return (
-    <FilterContainer>
+    <FilterContainer onClick={clickHandler}>
       <Icon />
       <Name />
     </FilterContainer>
