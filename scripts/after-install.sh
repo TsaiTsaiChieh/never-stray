d5bafeaa73898af09dcc985ff85f6f5f02579372
Tsai-Chieh <d0177381@gmail.com>
2022-04-09 18:39:26 +0800

    :bug: fix missing blank key in the docker build command

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,6 +1,6 @@
 echo "After install"
 cd /home/ubuntu/never-stray
-docker build --cache-from=frontend-t 'frontend' ./frontend
+docker build --cache-from=frontend -t 'frontend' ./frontend
 docker build --cache-from=backend -t 'backend' ./backend
 docker build --cache-from=backend -t 'cron' ./backend
 docker build --cache-from=nginx -t 'nginx' ./nginx
