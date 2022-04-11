6e64d8d11d7f6c999c00421a6b6a01cb94989f9e
Tsai-Chieh <d0177381@gmail.com>
2022-04-11 18:36:55 +0800

    :tada: init KeywordSearch component

--- frontend/src/pages/PetList/Search/KeywordSearch.tsx
+++ frontend/src/pages/PetList/Search/KeywordSearch.tsx
@@ -1,12 +1,42 @@
-import styled from 'styled-components'
+import {useAppDispatch, useAppSelector} from '../../../store/hooks'
+import {
+  closeKeywordSearch,
+  keywordSearchOnClick,
+} from '../../../store/reducers/uiSlice'
+import {
+  CloseKeywordSearchButton,
+  KeywordSearchButton,
+  SearchIcon,
+  TextField,
+  TextFieldGroup,
+} from '../../../styled/PetList/Menu'
 
-const StyledKeywordSearch = styled.img`
-content: url('/images/Menu/keyword-search.svg');
-`
 const KeywordSearch = () => {
+  const {keywordSearchTextFieldIsShow} = useAppSelector((state) => state.ui)
+
   return (
-    <StyledKeywordSearch />
+    <TextFieldGroup id='TextFieldGroup' isShow={keywordSearchTextFieldIsShow}>
+      <TextField placeholder='關鍵字、顏色、性別' />
+      <SearchIcon id='SearchIcon' />
+    </TextFieldGroup>
   )
 }
 
-export default KeywordSearch
+const KeywordSearchBtn = () => {
+  const {keywordSearchTextFieldIsShow, closeTextSearchIsShow} =
+    useAppSelector((state) => state.ui)
+  const dispatch = useAppDispatch()
+  return (
+    <>
+      <KeywordSearchButton
+        isShow={!keywordSearchTextFieldIsShow}
+        onClick={() => dispatch(keywordSearchOnClick())}
+      />
+      <CloseKeywordSearchButton
+        isShow={closeTextSearchIsShow}
+        onClick={() => dispatch(closeKeywordSearch())}
+      />
+    </>
+  )
+}
+export {KeywordSearch, KeywordSearchBtn}
