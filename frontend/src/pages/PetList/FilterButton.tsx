9acac161a17eec5c4e134448b5683a55aaac47d3
Tsai-Chieh <d0177381@gmail.com>
2022-04-11 21:20:05 +0800

    :sparkles: FilterContainer add isShow prop

--- frontend/src/pages/PetList/FilterButton.tsx
+++ frontend/src/pages/PetList/FilterButton.tsx
@@ -1,12 +1,14 @@
-import {useAppDispatch} from '../../store/hooks'
+import {useAppDispatch, useAppSelector} from '../../store/hooks'
 import {filterBtnOnClick} from '../../store/reducers/uiSlice'
 import {FilterContainer, Icon, Name} from '../../styled/PetList/FilterButton'
 
 export const FilterButton = () => {
+  const {filterBtnIsShow} = useAppSelector((state) => state.ui)
   const useDispatch = useAppDispatch()
 
   return (
-    <FilterContainer onClick={() => useDispatch(filterBtnOnClick())}>
+    <FilterContainer isShow={filterBtnIsShow}
+      onClick={() => useDispatch(filterBtnOnClick())}>
       <Icon />
       <Name />
     </FilterContainer>
