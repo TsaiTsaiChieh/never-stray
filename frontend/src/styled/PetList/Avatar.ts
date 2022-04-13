87040eec8e504881f1a87c8effd79f4d4e7476a4
Tsai-Chieh <d0177381@gmail.com>
2022-04-13 20:55:22 +0800

    :lipstick: add skeleton loader, ref from https://blog.prototypr.io/how-to-create-placeholder-loading-animations-in-css-2334255aa74c

--- frontend/src/styled/PetList/Avatar.ts
+++ frontend/src/styled/PetList/Avatar.ts
@@ -1,8 +1,9 @@
 import styled from 'styled-components'
 
 import {MEDIA_TABLET} from '../../constants/Mixin'
+import {colors} from '../../constants/Variables'
 
-export const StyledAvatar = styled.div`
+export const StyledAvatar = styled.div<{isLoading: boolean}>`
   cursor: pointer;
   position: relative;
   object-fit: cover;
@@ -16,6 +17,28 @@ export const StyledAvatar = styled.div`
   mask: url("/images/PetList/pet-mask.svg") no-repeat center center;
   -webkit-mask-size: contain;
   mask-size: contain;
+  &::before {
+    content: "";
+    display: ${(props) => props.isLoading ? 'block' : 'none'};
+    position: absolute;
+    height: 100%;
+    width: 150px;
+    background: linear-gradient(
+      to right,
+      transparent 0%,
+      ${colors.gray.i600} 50%,
+      transparent 100%
+    );
+    animation: load 1s cubic-bezier(0.4, 0, 0.2, 1) infinite;
+  }
+  @keyframes load {
+    from {
+      left: -150px;
+    }
+    to {
+      left: 100%;
+    }
+  }
   ${MEDIA_TABLET} {
     width: 10rem;
     height: 9.5rem;
