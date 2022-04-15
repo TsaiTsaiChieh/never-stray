2fa1fbf9f9983715d0fe6339bc95cdf656be3a68
Tsai-Chieh <d0177381@gmail.com>
2022-04-15 20:26:04 +0800

    :art: format

--- frontend/src/styled/PetList/NotFound.ts
+++ frontend/src/styled/PetList/NotFound.ts
@@ -2,9 +2,9 @@ import styled from 'styled-components'
 import {MEDIA_TABLET} from '../../constants/Mixin'
 
 export const StyledNotFound = styled.img`
-content:url('/images/PetList/not-found.png');
-width: 300px;
-${MEDIA_TABLET} {
-  width: 400px;
-}
+  content: url("/images/PetList/not-found.png");
+  width: 300px;
+  ${MEDIA_TABLET} {
+    width: 400px;
+  }
 `
