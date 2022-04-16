a20a3cd9d7c72dd55912137912db6005cc3706ba
Tsai-Chieh <d0177381@gmail.com>
2022-04-17 01:19:49 +0800

    :lipstick: tune styled-component in gallery

--- frontend/src/styled/PetProfile/Gallery.ts
+++ frontend/src/styled/PetProfile/Gallery.ts
@@ -1,13 +1,18 @@
 import styled from 'styled-components'
 
+import {MEDIA_DESKTOP, MEDIA_TABLET} from '../../constants/Mixin'
 import {colors} from '../../constants/Variables'
 import {AdoptedImg} from '../PetList/Profile'
 
 export const Container = styled.div`
   display: flex;
+  position: relative;
   width: 100%;
   flex-direction: column;
   align-items: center;
+  ${MEDIA_TABLET} {
+    flex-direction: row;
+  }
 `
 export const Image = styled.img<{image: string}>`
   width: 50vh;
@@ -19,6 +24,20 @@ export const Image = styled.img<{image: string}>`
   box-shadow: 0px 0px 8px ${colors.gray.t100};
 `
 export const AdoptedImgInProfile = styled(AdoptedImg)`
-  top: 50px;
-  left: 0px;
+  width: 100px;
+  height: 100px;
+  top: -10px;
+  left: -20px;
+  ${MEDIA_TABLET} {
+    width: 140px;
+    height: 140px;
+    top: 10px;
+    left: -50px;
+  }
+  ${MEDIA_DESKTOP} {
+    width: 168px;
+    height: 168px;
+    top: 0px;
+    left: -65px;
+  }
 `
