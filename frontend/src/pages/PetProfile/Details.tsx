5d9bf0374d276e8f1ae6e5802c428140e5e41e0c
Tsai-Chieh <d0177381@gmail.com>
2022-04-17 17:50:22 +0800

    :sparkles: finish Details component

--- frontend/src/pages/PetProfile/Details.tsx
+++ frontend/src/pages/PetProfile/Details.tsx
@@ -1,6 +1,39 @@
-const Details = () => {
+import {PetRef} from '../../constants/EnumType'
+import {
+  AccessNumber,
+  Container,
+  Description,
+  DescriptionContainer,
+  DescriptionTitle,
+  Title,
+  UpdateTime,
+} from '../../styled/PetProfile/Details'
+import Feature from './Feature'
+
+interface Props {
+  pet: IPet;
+}
+const Details = ({pet}: Props) => {
+  const accessNum: string =
+    pet.ref === PetRef.GOV ?
+      `${pet.sub_id} ${pet.accept_num}`.toString() :
+      pet.sub_id.toString()
   return (
-    <div>Details</div>
+    <Container petRef={pet.ref}>
+      <Title>{pet.ref === PetRef.MAP ? pet.title : pet.shelter_name}</Title>
+      <UpdateTime updatedAt={pet.updated_at.substring(0, 10)} />
+      <AccessNumber accessNum={accessNum} />
+      <Feature
+        sex={pet.sex}
+        age={pet.age}
+        city={pet.city_name}
+        ligation={pet.ligation}
+      />
+      <DescriptionContainer>
+        <DescriptionTitle>介紹描述</DescriptionTitle>
+        <Description>{pet.remark}</Description>
+      </DescriptionContainer>
+    </Container>
   )
 }
 
