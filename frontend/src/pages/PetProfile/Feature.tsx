3ddbe1c50426f792bb5a6edac03486d54769ffdd
Tsai-Chieh <d0177381@gmail.com>
2022-04-17 17:52:48 +0800

    :art: finish Feature component

--- frontend/src/pages/PetProfile/Feature.tsx
+++ frontend/src/pages/PetProfile/Feature.tsx
@@ -1,6 +1,41 @@
-function Feature() {
+import {
+  Container,
+  Frame,
+  Sex,
+  Title,
+  Value,
+} from '../../styled/PetProfile/Feature'
+import {
+  ligationConverter,
+  petAgeConverter,
+} from '../../utils/value-converter'
+
+interface Props {
+  sex: PetSexType;
+  age: PetAgeType;
+  city: string;
+  ligation: TernaryType;
+}
+const Feature = ({sex, age, city, ligation}: Props) => {
   return (
-    <div>Feature</div>
+    <Container>
+      <Frame>
+        <Title>性別</Title>
+        <Sex sex={sex}></Sex>
+      </Frame>
+      <Frame>
+        <Title>年紀</Title>
+        <Value>{petAgeConverter(age)}</Value>
+      </Frame>
+      <Frame>
+        <Title>地區</Title>
+        <Value>{city}</Value>
+      </Frame>
+      <Frame>
+        <Title>結紮</Title>
+        <Value>{ligationConverter(ligation)}</Value>
+      </Frame>
+    </Container>
   )
 }
 
