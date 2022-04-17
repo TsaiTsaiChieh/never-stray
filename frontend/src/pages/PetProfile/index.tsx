57f7b620fcb9dde7f8d744894886f15c78e14960
Tsai-Chieh <d0177381@gmail.com>
2022-04-17 17:54:04 +0800

    :sparkles: add #PetProfile container

--- frontend/src/pages/PetProfile/index.tsx
+++ frontend/src/pages/PetProfile/index.tsx
@@ -1,9 +1,32 @@
 import {useParams} from 'react-router-dom'
 
+import {useGetPetByIdQuery} from '../../api/pets'
+import DogWalking from '../../components/DogWalking'
+import Footer from '../../components/Footer'
+import Header from '../../components/Header'
+import Details from './Details'
+import Gallery from './Gallery'
+
 const PetProfile = () => {
   const {id} = useParams()
+  const {data, isLoading, isSuccess} = useGetPetByIdQuery(id!)
+
   return (
-    <div>{id}</div>
+    <>
+      <Header />
+      {isLoading && <DogWalking />}
+      <div id='PetProfile'>
+        {isSuccess &&
+          data &&
+          [data].map((ele) => (
+            <>
+              <Gallery images={ele.image} status={ele.status} />
+              <Details pet={ele} />
+            </>
+          ))}
+      </div>
+      <Footer />
+    </>
   )
 }
 
