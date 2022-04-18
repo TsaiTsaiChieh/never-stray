0a114313610ee9cbc6b14bda71f4ac8fa440c509
Tsai-Chieh <d0177381@gmail.com>
2022-04-18 19:50:48 +0800

    :lipstick: fixed the close button when scroll down

--- frontend/src/styled/Base/CloseButton.ts
+++ frontend/src/styled/Base/CloseButton.ts
@@ -3,11 +3,12 @@ import styled from 'styled-components'
 import {colors} from '../../constants/Variables'
 
 export const CloseButton = styled.div`
-  position: absolute;
+  position: fixed;
   width: 30px;
   height: 30px;
-  top: 20px;
-  right: 20px;
+  top: 70px;
+  right: 35px;
+  z-index: 30;
   cursor: pointer;
   &::after,
   &::before {
