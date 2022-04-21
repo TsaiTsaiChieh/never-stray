b1bc4767bd9ebcb6009be8cc02c9fd77928a5cfa
Tsai-Chieh <d0177381@gmail.com>
2022-04-22 00:01:59 +0800

    :tada: init Login component

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,12 @@
+import {Container, UserAvatar, UserName} from '../styled/Login'
+
+const Login = () => {
+  return (
+    <Container>
+      <UserAvatar />
+      <UserName>登入</UserName>
+    </Container>
+  )
+}
+
+export default Login
