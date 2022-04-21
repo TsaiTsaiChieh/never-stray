fea693a41c5e27470f48dcdd205ebbcf03c5edd7
Tsai-Chieh <d0177381@gmail.com>
2022-04-21 23:39:43 +0800

    :sparkles: add /auth/google/callback API

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,6 +1,9 @@
 /* eslint-disable require-jsdoc */
+import axios from 'axios'
 import {Request, Response} from 'express'
 import {Controller, Get, Req, Res} from 'routing-controllers'
+
+import {AxiosError} from '../utils/app-error'
 
 @Controller('/auth')
 export class AuthController {
@@ -14,5 +17,30 @@ export class AuthController {
       'response_type=code&' +
       `client_id=${GOOGLE_CLIENT_ID}`
     return res.json({redirect_url: googleOauthUrl})
+  }
+
+  @Get('/google/callback')
+  async googleCallback(@Req() req: Request, @Res() res: Response) {
+    const {GOOGLE_REDIRECT_URL, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET} =
+      process.env
+    const {code} = req.query
+    try {
+      const tokenRes = await axios({
+        method: 'POST',
+        url: 'https://www.googleapis.com/oauth2/v4/token',
+        params: {
+          code,
+          client_id: GOOGLE_CLIENT_ID,
+          client_secret: GOOGLE_CLIENT_SECRET,
+          grant_type: 'authorization_code',
+          redirect_uri: GOOGLE_REDIRECT_URL,
+        },
+      })
+      const tokenOption: GoogleTokenOptionType = tokenRes.data
+      return res.json(tokenOption)
+    } catch (error) {
+      const err = new AxiosError(error.stack)
+      return res.status(err.code).json(err)
+    }
   }
 }
