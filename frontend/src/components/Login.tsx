390087515ab35c155b23af6a5d3c35f83018f985
Tsai-Chieh <d0177381@gmail.com>
2022-04-22 21:04:21 +0800

    :alembic: import GoogleLogin component

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,11 +1,23 @@
+import GoogleLogin from 'react-google-login'
+
 import {Container, UserAvatar, UserName} from '../styled/Login'
 
+const responseGoogle = (response: any) => {
+  console.log(response)
+}
 const Login = () => {
   return (
     <Container>
       <UserAvatar />
-      <UserName>登入</UserName>
-    </Container>
+      <GoogleLogin
+        render={(renderProps) => (
+          <UserName onClick={renderProps.onClick}>登入</UserName>
+        )}
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID!}
+      />
+    </Container >
   )
 }
 
