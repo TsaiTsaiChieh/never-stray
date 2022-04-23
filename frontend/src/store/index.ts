0b1dfb4ad86dbe1ed995c9d3fafa5cc647cf0738
Tsai-Chieh <d0177381@gmail.com>
2022-04-23 21:17:59 +0800

    :sparkles: reducer import authApi

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,4 +1,5 @@
 import {configureStore} from '@reduxjs/toolkit'
+import {authApi} from '../api/auth'
 
 import {petsApi} from '../api/pets'
 import enumReducer from './reducers/enumSlice'
@@ -8,12 +9,13 @@ import uiReducer from './reducers/uiSlice'
 const store = configureStore({
   reducer: {
     [petsApi.reducerPath]: petsApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
     petList: petListReducer,
     enum: enumReducer,
     ui: uiReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(petsApi.middleware),
+    getDefaultMiddleware().concat(petsApi.middleware, authApi.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
