62a0300ed1466fc7284747abfb19a33304310a3e
Tsai-Chieh <d0177381@gmail.com>
2022-04-24 01:32:51 +0800

    :sparkles: add upsert func in User repository

--- backend/src/repository/user.repository.ts
+++ backend/src/repository/user.repository.ts
@@ -1,5 +1,5 @@
 import {User} from '../entity/user.entity'
-
+import {DBError} from '../utils/app-error'
 import {BasicRepository} from '../utils/basic-repository'
 
 /** @class UserRepository */
@@ -7,5 +7,27 @@ export class UserRepository extends BasicRepository<User> {
   /** @constructor */
   constructor() {
     super(User)
+  }
+
+  /**
+   * Upsert user data
+   *
+   * @param  {string} name The user's full name
+   * @param  {string} email The user's email address
+   * @param  {string} picture The URL of the user's profile picture
+   * @return {Promise<void>}
+   *
+   */
+  async upsert({name, email, picture}: UserType): Promise<void> {
+    try {
+      const result = await this.repository.findOne({email})
+
+      result ?
+        await this.repository.update({name, picture}, {email}) :
+        await this.repository.save({name, email, picture})
+      return Promise.resolve()
+    } catch (error) {
+      return Promise.reject(new DBError(error.stack))
+    }
   }
 }
