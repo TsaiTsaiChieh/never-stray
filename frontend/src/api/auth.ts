d73a3ee51bf473539f3f2aac71eef3e77ad63d96
Tsai-Chieh <d0177381@gmail.com>
2022-04-23 20:23:13 +0800

    :sparkles: add googleLogin mutation API

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,18 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+export const authApi = createApi({
+  reducerPath: 'auth',
+  baseQuery: fetchBaseQuery({baseUrl: process.env.REACT_APP_API_URL}),
+  endpoints: (builder) => ({
+    googleLogin: builder.mutation<UserInfoType, {token: string}>({
+      query: (token) => (
+        {
+          url: `/auth/google-login`,
+          method: 'POST',
+          body: token,
+        }),
+    }),
+  }),
+})
+
+export const {useGoogleLoginMutation} = authApi
