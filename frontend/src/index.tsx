6436bff4e0a0b35f99a9d8622a26c48a4d7a2310
Tsai-Chieh <d0177381@gmail.com>
2022-04-25 21:20:33 +0800

    :sparkles: import redux-persist, ref from https://dev.to/dawnind/persist-redux-state-with-redux-persist-3k0d

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,14 +4,20 @@ import './styles/App.scss'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {BrowserRouter} from 'react-router-dom'
+import {persistStore} from 'redux-persist'
+import {PersistGate} from 'redux-persist/integration/react'
 
 import App from './App'
 import store from './store'
 
+const persistor = persistStore(store)
+
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,23 +1,38 @@
-import {configureStore} from '@reduxjs/toolkit'
-import {authApi} from '../api/auth'
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
+import {configureStore} from '@reduxjs/toolkit'
+
+import {authApi} from '../api/auth'
 import {petsApi} from '../api/pets'
-import enumReducer from './reducers/enumSlice'
-import petListReducer from './reducers/petListSlice'
-import uiReducer from './reducers/uiSlice'
+import rootReducer from './rootReducer'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: [authApi.reducerPath, 'enum'],
+  blacklist: [petsApi.reducerPath, 'petList', 'ui'],
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const store = configureStore({
-  reducer: {
-    [petsApi.reducerPath]: petsApi.reducer,
-    [authApi.reducerPath]: authApi.reducer,
-    petList: petListReducer,
-    enum: enumReducer,
-    ui: uiReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(petsApi.middleware, authApi.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(petsApi.middleware, authApi.middleware),
 })
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 export default store
