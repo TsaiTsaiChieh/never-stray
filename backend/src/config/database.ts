53b27680c303b63ecd85a0e721914efe0b4c220a
Tsai-Chieh <d0177381@gmail.com>
2022-04-25 18:05:28 +0800

    :sparkles: import tracking entity

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -4,6 +4,7 @@ import {Connection, createConnection} from 'typeorm'
 import {Area} from '../entity/area.entity'
 import {Pet} from '../entity/pet.entity'
 import {Shelter} from '../entity/shelter.entity'
+import {Tracking} from '../entity/tracking.entity'
 import {User} from '../entity/user.entity'
 import {DBError} from '../utils/app-error'
 
@@ -19,7 +20,7 @@ export const connection = async (
       username: process.env.MYSQL_USERNAME,
       password: process.env.MYSQL_ROOT_PASSWORD,
       database: process.env.MYSQL_DATABASE,
-      entities: [Area, Pet, Shelter, User],
+      entities: [Area, Pet, Shelter, User, Tracking],
       synchronize: false,
       logging: false,
     })
