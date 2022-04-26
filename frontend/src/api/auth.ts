e096fe5720e8d8566e20c28210a7a83c293f88cd
Tsai-Chieh <d0177381@gmail.com>
2022-04-26 21:30:13 +0800

    :truck: rename reducerPath from auth to authApi

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,7 +1,7 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 export const authApi = createApi({
-  reducerPath: 'auth',
+  reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({baseUrl: process.env.REACT_APP_API_URL}),
   endpoints: (builder) => ({
     googleLogin: builder.mutation<UserInfoType, {token: string}>({
