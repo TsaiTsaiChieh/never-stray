62faeeff42aa9019082c3e7f6fd6b3a1778fcf71
Tsai-Chieh <d0177381@gmail.com>
2022-04-29 00:24:49 +0800

    :sparkles: add DELETE /tracking API

--- backend/src/controllers/tracking.controller.ts
+++ backend/src/controllers/tracking.controller.ts
@@ -1,7 +1,14 @@
 /* eslint-disable require-jsdoc */
 import {Request, Response} from 'express'
 import httpStatus from 'http-status'
-import {Controller, Post, Req, Res, UseBefore} from 'routing-controllers'
+import {
+  Controller,
+  Delete,
+  Post,
+  Req,
+  Res,
+  UseBefore,
+} from 'routing-controllers'
 import safeAwait from 'safe-await'
 
 import {TrackingModel} from '../models/tracking.model'
@@ -9,6 +16,7 @@ import {toggleTrackingSchema} from '../schemas/tracking.schema'
 import {ajv} from '../utils/ajv-util'
 import {MustLogin} from '../utils/middlewares'
 
+@UseBefore(MustLogin)
 @Controller('/tracking')
 export class TrackingController {
   private model: TrackingModel
@@ -17,7 +25,6 @@ export class TrackingController {
     this.model = new TrackingModel
   }
 
-  @UseBefore(MustLogin)
   @Post('/')
   async postTracking(@Req() req: Request, @Res() res: Response) {
     const valid: boolean = ajv.validate(toggleTrackingSchema, req.body)
@@ -27,5 +34,16 @@ export class TrackingController {
 
     if (error) return res.status(error.code).json(error)
     return res.status(httpStatus.CREATED).send('ok')
+  }
+
+  @Delete('/')
+  async deleteTracking(@Req() req: Request, @Res() res: Response) {
+    const valid: boolean = ajv.validate(toggleTrackingSchema, req.body)
+    if (!valid) return res.status(httpStatus.BAD_REQUEST).json(ajv.errors)
+    const [error, _]: [ErrorType, void] = await safeAwait(
+      this.model.removeTracking(req.body.pet_id, req.userId))
+
+    if (error) return res.status(error.code).json(error)
+    return res.send('ok')
   }
 }
