7d0e49f664c9ade01644fe48d7dc6598cad0307c
Tsai-Chieh <d0177381@gmail.com>
2022-04-28 23:41:35 +0800

    :recycle: update baseUrl from process.env

--- frontend/src/api/pets.ts
+++ frontend/src/api/pets.ts
@@ -1,10 +1,8 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
-const {REACT_APP_API_URL} = process.env
-
 export const petsApi = createApi({
   reducerPath: 'pets',
-  baseQuery: fetchBaseQuery({baseUrl: REACT_APP_API_URL}),
+  baseQuery: fetchBaseQuery({baseUrl: process.env.REACT_APP_API_URL}),
   endpoints: (builder) => ({
     getPetById: builder.query<IPet, string>({
       query: (id) => `/pets/${id}`,
