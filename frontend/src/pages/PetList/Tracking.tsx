b1068527db7ef27ed9b72d5b24d10d2db33b1fe1
Tsai-Chieh <d0177381@gmail.com>
2022-04-29 00:38:03 +0800

    :tada: init Tracking component and add toggle tracking btn func

---
+++ frontend/src/pages/PetList/Tracking.tsx
@@ -0,0 +1,30 @@
+import {
+  useAddTrackingApiMutation,
+  useRemoveTrackingApiMutation,
+} from '../../api/tracking'
+import {useAppDispatch, useAppSelector} from '../../store/hooks'
+import {togglePetTracking} from '../../store/reducers/petListSlice'
+import {TrackingOrNot} from '../../styled/PetList/Tracking'
+
+interface Props {
+  id: number;
+  tracking: boolean;
+  idx: number;
+}
+const Tracking = ({id, tracking, idx}: Props) => {
+  const {isLogin} = useAppSelector((state) => state.auth)
+  const dispatch = useAppDispatch()
+  const [addTrackingApi] = useAddTrackingApiMutation()
+  const [removeTrackingApi] = useRemoveTrackingApiMutation()
+
+  const toggleTracking = () => {
+    if (!isLogin) return
+    dispatch(togglePetTracking({idx}))
+    if (tracking) removeTrackingApi({pet_id: id})
+    else addTrackingApi({pet_id: id})
+  }
+
+  return <TrackingOrNot onClick={() => toggleTracking()} tracking={tracking} />
+}
+
+export default Tracking
