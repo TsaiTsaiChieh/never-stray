be1d4a52d0675b1481668abbf0b7c6b2e18cc7e0
Tsai-Chieh <d0177381@gmail.com>
2022-04-29 18:38:48 +0800

    :art: format

--- frontend/src/api/pets.ts
+++ frontend/src/api/pets.ts
@@ -2,10 +2,12 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 export const petsApi = createApi({
   reducerPath: 'pets',
-  baseQuery: fetchBaseQuery({baseUrl: process.env.REACT_APP_API_URL}),
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${process.env.REACT_APP_API_URL}/pets/`,
+  }),
   endpoints: (builder) => ({
     getPetById: builder.query<IPet, string>({
-      query: (id) => `/pets/${id}`,
+      query: (id) => `${id}`,
     }),
   }),
 })
