852c2d68656bf77ed69483ae09b58fc0bafc393e
Tsai-Chieh <d0177381@gmail.com>
2022-04-30 13:21:53 +0800

    :sparkles: add getPetsByFilters query api

--- frontend/src/api/pets.ts
+++ frontend/src/api/pets.ts
@@ -1,15 +1,58 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
+import {PetKind} from '../constants/EnumType'
+import {RootState} from '../store'
+import {concatUrl} from '../utils/helper'
+
 export const petsApi = createApi({
   reducerPath: 'pets',
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.REACT_APP_API_URL}/pets/`,
+    baseUrl: process.env.REACT_APP_API_URL,
+    prepareHeaders: (headers, {getState}) => {
+      const token = (getState() as RootState).auth.userData?.token
+      if (token) headers.set('authorization', `Bearer ${token}`)
+      return headers
+    },
   }),
   endpoints: (builder) => ({
     getPetById: builder.query<IPet, string>({
-      query: (id) => `${id}`,
+      query: (id) => `/pets/${id}`,
+    }),
+    getPetsByFilters: builder.query<PetsAPIType, SearchPetFilters>({
+      query: ({
+        page,
+        limit,
+        status,
+        ref,
+        city_id,
+        shelter_id,
+        kind,
+        sex,
+        color,
+        age,
+        region,
+        ligation,
+        keyword,
+        order_key,
+        ascend,
+      }) => {
+        let url = `/pets?page=${page}&limit=${limit}`
+        url += concatUrl(status, 'status')
+        url += concatUrl(ref, 'ref')
+        url += concatUrl(city_id, 'city_id')
+        url += concatUrl(shelter_id, 'shelter_id')
+        if (kind !== PetKind.ALL) url += `$kind[]=${kind}`
+        url += concatUrl(sex, 'sex')
+        url += concatUrl(color, 'color')
+        url += concatUrl(age, 'age')
+        url += concatUrl(region, 'region')
+        url += concatUrl(ligation, 'ligation')
+        if (keyword) url += `&keyword=${keyword}`
+        if (order_key) url += `&order_key=${order_key}&ascend=${ascend}`
+        return url
+      },
     }),
   }),
 })
 
-export const {useGetPetByIdQuery} = petsApi
+export const {useGetPetByIdQuery, useGetPetsByFiltersQuery} = petsApi
