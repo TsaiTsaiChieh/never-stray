152804c62cf9af2c1283091919a142e77c2931ac
Tsai-Chieh <d0177381@gmail.com>
2022-04-30 19:21:49 +0800

    :alembic: replace createAsyncThunk(getPets) to createApi(useGetPetsByFiltersQuery)

--- frontend/src/pages/PetList/index.tsx
+++ frontend/src/pages/PetList/index.tsx
@@ -1,11 +1,9 @@
-import {useEffect} from 'react'
-
+import {useGetPetsByFiltersQuery} from '../../api/pets'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 import Mask from '../../components/Mask'
 import Pagination from '../../components/Pagination'
 import {useAppDispatch, useAppSelector} from '../../store/hooks'
-import {getPets} from '../../store/reducers/petListSlice'
 import Banner from './Banner'
 import Menu from './Menu'
 import NotFound from './NotFound'
@@ -16,10 +14,7 @@ import SearchBoard from './Search'
 export const PetList = () => {
   const dispatch = useAppDispatch()
   const petState = useAppSelector((state) => state.petList)
-
-  useEffect(() => {
-    dispatch(getPets({...petState.filters}))
-  }, [])
+  const {data} = useGetPetsByFiltersQuery({...petState.filters})
 
   return (
     <>
@@ -34,9 +29,9 @@ export const PetList = () => {
           <Banner />
           <OrderFilter />
           <div id='PetList'>
-            {petState.pets.length ? (
-              petState.pets &&
-              petState.pets.map((ele: IPet, idx: number) => (
+            {data && data.pets.length ? (
+              data.pets &&
+              data.pets.map((ele: IPet, idx: number) => (
                 <Profile key={ele.id} pet={ele} idx={idx} />
               ))
             ) : (
