1ceac899ad57ef2ddd5684b7f4601c438d38d5cf
Tsai-Chieh <d0177381@gmail.com>
2022-05-02 17:27:17 +0800

    :tada: init CleanupFilter component

---
+++ frontend/src/pages/PetList/Search/CleanupFilters.tsx
@@ -0,0 +1,33 @@
+import {PetKind, PetStatus} from '../../../constants/EnumType'
+import {useAppDispatch} from '../../../store/hooks/index'
+import {updateFilters} from '../../../store/reducers/petListSlice'
+import {CleanupFiltersBtn} from '../../../styled/PetList/SearchBoard'
+
+const CleanupFilters = () => {
+  const dispatch = useAppDispatch()
+
+  const onClick = () => {
+    dispatch(updateFilters({
+      status: [PetStatus.OPEN],
+      ref: [],
+      city_id: [],
+      shelter_id: [],
+      kind: PetKind.ALL,
+      age: [],
+      sex: [],
+      region: [],
+      ligation: [],
+      color: [],
+      tracking: false,
+      limit: 18,
+      page: 1,
+      ascend: true,
+    }))
+  }
+
+  return (
+    <CleanupFiltersBtn onClick={() => onClick()}>清除條件</CleanupFiltersBtn>
+  )
+}
+
+export default CleanupFilters
