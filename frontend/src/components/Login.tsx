65997bb2787281daae3b30285ac4cf2ac0fcfa71
Tsai-Chieh <d0177381@gmail.com>
2022-05-06 22:16:34 +0800

    :recycle: load from custom GoogleLoginBtn component

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,28 +1,11 @@
-import GoogleLogin, {
-  GoogleLoginResponse,
-  GoogleLoginResponseOffline,
-} from 'react-google-login'
-
-import {useGoogleLoginMutation} from '../services/api'
 import {useAppDispatch, useAppSelector} from '../store/hooks'
 import {logout} from '../store/reducers/authSlice'
 import {Container, LoginBtn, UserAvatar} from '../styled/Login'
+import GoogleLoginBtn from './GoogleLoginBtn'
 
 const Login = () => {
   const {isLogin, userData} = useAppSelector((state) => state.auth)
   const dispatch = useAppDispatch()
-  const [googleLogin] = useGoogleLoginMutation()
-
-  const handleLogin = (
-    res: GoogleLoginResponse | GoogleLoginResponseOffline,
-  ) => {
-    // Callback will return the GoogleAuth object
-    if ('tokenId' in res) {
-      const tokenId = res.tokenId
-      // Carry token to backend
-      googleLogin({token: tokenId})
-    }
-  }
 
   const handleLogout = () => {
     dispatch(logout())
@@ -34,13 +17,9 @@ const Login = () => {
       {isLogin ? (
         <LoginBtn onClick={handleLogout}>登出</LoginBtn>
       ) : (
-        <GoogleLogin
-          render={(renderProps) => (
-            <LoginBtn onClick={renderProps.onClick}>登入</LoginBtn>
-          )}
-          onSuccess={handleLogin}
-          // TODO Should add handleFailure function
-          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID!}
+        <GoogleLoginBtn
+          Component={LoginBtn}
+          buttonName='登入'
         />
       )}
     </Container>
