ad91cf9cd72a966feaa3cf74ddcddbe17463d76b
Tsai-Chieh <d0177381@gmail.com>
2022-05-07 19:53:42 +0800

    :bug: force re-fetches the pet data for tracking state when user logout

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,3 +1,4 @@
+import {api} from '../services/api'
 import {useAppDispatch, useAppSelector} from '../store/hooks'
 import {logout} from '../store/reducers/authSlice'
 import {Container, LoginBtn, UserAvatar} from '../styled/Login'
@@ -6,9 +7,17 @@ import GoogleLoginBtn from './GoogleLoginBtn'
 const Login = () => {
   const {isLogin, userData} = useAppSelector((state) => state.auth)
   const dispatch = useAppDispatch()
+  const {filters} = useAppSelector((state) => state.petList)
 
   const handleLogout = () => {
     dispatch(logout())
+    // force re-fetches the pet data for tracking state
+    dispatch(
+      api.endpoints.getPetsByFilters.initiate(
+        {...filters},
+        {forceRefetch: true},
+      ),
+    )
   }
 
   return (
@@ -17,10 +26,7 @@ const Login = () => {
       {isLogin ? (
         <LoginBtn onClick={handleLogout}>登出</LoginBtn>
       ) : (
-        <GoogleLoginBtn
-          Component={LoginBtn}
-          buttonName='登入'
-        />
+        <GoogleLoginBtn Component={LoginBtn} buttonName='登入' />
       )}
     </Container>
   )
