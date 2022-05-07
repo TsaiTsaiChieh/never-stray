4592efe3ea3996a8ee7f4909398abdf266b92286
Tsai-Chieh <d0177381@gmail.com>
2022-05-07 21:43:07 +0800

    :lipstick: add ButtonContainer related styled-components

--- frontend/src/styled/PetProfile/Details.ts
+++ frontend/src/styled/PetProfile/Details.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components'
 
-import {MEDIA_DESKTOP} from '../../constants/Mixin'
+import {PetStatus} from '../../constants/EnumType'
+import {MEDIA_DESKTOP, MEDIA_TABLET} from '../../constants/Mixin'
 import {colors, sizes} from '../../constants/Variables'
 import {petStatusConverter} from '../../utils/value-converter'
+import {Button} from '../PetList/SearchBoard'
 
 const TextSetting = styled.div`
   font-size: ${sizes.s};
@@ -76,5 +78,67 @@ export const Description = styled.p`
   color: ${colors.gray.i300};
   line-height: 1.5;
   font-size: 15px;
-  margin: 10px 0 20px;
+  margin-top: 10px;
+`
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 10px 0;
+  ${MEDIA_TABLET} {
+    justify-content: flex-start;
+  }
+`
+export const ContactUs = styled.button<{status: PetStatusType}>`
+  width: auto;
+  height: 40px;
+  flex-grow: 1;
+  letter-spacing: 1px;
+  font-size: ${sizes.xs};
+  background: ${(props) =>
+    props.status === PetStatus.ADOPTED ?
+      colors.gray.i300 :
+      colors.tiffany.i300};
+  color: ${colors.white.i100};
+  border: none;
+  border-radius: 5px;
+  &:hover {
+    background: ${(props) =>
+    props.status === PetStatus.ADOPTED ?
+      colors.gray.i400 :
+      colors.tiffany.i500};
+  }
+  ${MEDIA_TABLET} {
+    flex-grow: 1;
+  }
+  ${MEDIA_DESKTOP} {
+    max-width: 300px;
+  }
+`
+export const AddTracking = styled(Button)`
+  width: 10vw;
+  margin-left: 10px;
+  float: right;
+  border: 1px solid ${colors.tiffany.i300};
+  color: ${colors.gray.i400};
+  &:hover {
+    color: ${colors.tiffany.i300};
+  }
+  ${MEDIA_TABLET} {
+    flex-grow: 1;
+    margin-left: 20px;
+  }
+  ${MEDIA_DESKTOP} {
+    max-width: 300px;
+  }
+`
+export const TrackingIcon = styled.img<{tracking: boolean}>`
+  content: ${(props) =>
+    props.tracking ?
+      'url(/images/PetProfile/tracking.svg)' :
+      'url(/images/PetProfile/untracking.svg)'};
+  width: 20px;
+  height: 17.5px;
+  ${MEDIA_TABLET} {
+    margin-right: 14px;
+  }
 `
