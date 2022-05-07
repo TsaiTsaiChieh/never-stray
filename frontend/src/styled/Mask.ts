e67844d0f6b7761454dc1e7abd6374c025502faf
Tsai-Chieh <d0177381@gmail.com>
2022-05-07 15:45:52 +0800

    :lipstick: update z-index

--- frontend/src/styled/Mask.ts
+++ frontend/src/styled/Mask.ts
@@ -5,7 +5,7 @@ import {colors} from '../constants/Variables'
 export const StyledMask = styled.div<{isShow: boolean}>`
   display: ${(props) => (props.isShow ? 'block' : 'none')};
   position: fixed;
-  z-index: 1;
+  z-index: 11;
   width: 100%;
   height: 100%;
   background-color: ${colors.tiffany.i600};
