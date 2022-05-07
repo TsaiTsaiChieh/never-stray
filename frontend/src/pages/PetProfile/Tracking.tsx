17647d304ea8d6b089f3186748fbb046b9c8d16b
Tsai-Chieh <d0177381@gmail.com>
2022-05-08 00:35:41 +0800

    :sparkles: add toggle tracking logic and could refetch getPetById when isLogin state changing

--- frontend/src/pages/PetProfile/Tracking.tsx
+++ frontend/src/pages/PetProfile/Tracking.tsx
@@ -1,13 +1,40 @@
+import {useEffect} from 'react'
 import {isDesktop, isTablet} from 'react-device-detect'
 
+import {
+  api,
+  useAddTrackingMutation,
+  useRemoveTrackingMutation,
+} from '../../services/api'
+import {useAppDispatch, useAppSelector} from '../../store/hooks'
+import {updateShouldLoginWarningIsShow} from '../../store/reducers/uiSlice'
 import {TrackingBtn, TrackingIcon} from '../../styled/PetProfile/Details'
 
 interface Props {
+  id: number
   tracking: boolean
 }
-const Tracking = ({tracking}: Props) => {
+const Tracking = ({id, tracking}: Props) => {
+  const {isLogin} = useAppSelector((state) => state.auth)
+  const dispatch = useAppDispatch()
+  const [removeTracking] = useRemoveTrackingMutation()
+  const [addTracking] = useAddTrackingMutation()
+
+  useEffect(() => {
+    // force re-fetch when isLogin state changing
+    dispatch(
+      api.endpoints.getPetById.initiate(id.toString(), {forceRefetch: true}),
+    )
+  }, [isLogin])
+
+  const toggleTracking = () => {
+    if (!isLogin) return dispatch(updateShouldLoginWarningIsShow(true))
+    if (tracking) removeTracking({pet_id: id})
+    else addTracking({pet_id: id})
+  }
+
   return (
-    <TrackingBtn>
+    <TrackingBtn onClick={() => toggleTracking()}>
       <TrackingIcon tracking={tracking} />
       {isTablet || isDesktop ?
         `${tracking ? '已加入小窩' : '加入我的小窩'} ` :
