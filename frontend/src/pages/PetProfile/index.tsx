a716322b1adb5b04a6245ef265e39bcd3fb3278d
Tsai-Chieh <d0177381@gmail.com>
2022-05-08 16:16:07 +0800

    :sparkles: import ContactUs component and add control it logic

--- frontend/src/pages/PetProfile/index.tsx
+++ frontend/src/pages/PetProfile/index.tsx
@@ -1,21 +1,26 @@
 import {Fragment} from 'react'
 import {useParams} from 'react-router-dom'
 
-import {useGetPetByIdQuery} from '../../services/api'
 import DogWalking from '../../components/DogWalking'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
+import Mask from '../../components/Mask'
+import ShouldLoginWarning from '../../components/ShouldLoginWarning'
+import {useGetPetByIdQuery} from '../../services/api'
+import {useAppSelector} from '../../store/hooks'
+import ContactUs from './ContactUs'
 import Details from './Details'
 import Gallery from './Gallery'
-import ShouldLoginWarning from '../../components/ShouldLoginWarning'
-import Mask from '../../components/Mask'
 
 const PetProfile = () => {
   const {id} = useParams()
   const {data, isLoading, isSuccess} = useGetPetByIdQuery(id!)
+  const {contactUsIsShow} = useAppSelector((state) => state.ui)
 
-  return (
-    isLoading ? <DogWalking /> : <>
+  return isLoading ? (
+    <DogWalking />
+  ) : (
+    <>
       <Mask />
       <Header />
       <ShouldLoginWarning featureName='加入我的小窩' />
@@ -24,6 +29,7 @@ const PetProfile = () => {
           data &&
           [data].map((ele) => (
             <Fragment key={id}>
+              <ContactUs pet={ele} isShow={contactUsIsShow} />
               <Gallery images={ele.image} status={ele.status} />
               <Details pet={ele} />
             </Fragment>
