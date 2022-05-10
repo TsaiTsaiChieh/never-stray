a120ad8f5a331ab3cf67d027d011e9d922c46971
Tsai-Chieh <d0177381@gmail.com>
2022-05-10 18:21:11 +0800

    :tada: init RingLoader component

---
+++ frontend/src/components/RingLoader.tsx
@@ -0,0 +1,13 @@
+const RingLoader = () => {
+  return (
+    <svg
+      className='ring'
+      viewBox='25 25 50 50'
+      strokeWidth='5'
+    >
+      <circle cx='50' cy='50' r='20' />
+    </svg>
+  )
+}
+
+export default RingLoader
